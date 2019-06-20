27fd5b571028c0fe96cc60d3a11c503b8886db22
Yann Hourdel <yann@hourdel.fr>
2019-06-20 14:01:35 +0200

    configure SMTP for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    domain: ENV['SMTP_DOMAIN'],
+    authentication: :plain,
+    address: ENV['SMTP_ADDRESS'],
+    port: ENV['SMTP_PORT'],
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD']
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
