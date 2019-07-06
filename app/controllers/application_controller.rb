3b02804fd1a9a33e2b9d59f2bab2d4cec7a75b51
Yann Hourdel <yann@hourdel.fr>
2019-07-06 18:35:28 +0200

    Always use local time zone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_time_zone
+
   def status
     render plain: 'OK', status: 200
   end
@@ -7,5 +10,13 @@ class ApplicationController < ActionController::Base
 
   def user_for_paper_trail
     admin_user_signed_in? ? [current_admin_user.id, current_admin_user.email].join(':') : '0:anonymous'
+  end
+
+  private
+
+  def set_time_zone
+    if time_zone = cookies[:time_zone]
+      Time.zone = ActiveSupport::TimeZone[time_zone]
+    end
   end
 end
