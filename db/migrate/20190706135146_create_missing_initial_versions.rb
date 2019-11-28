9d274844412191e264921cf62535ae4e6fe9da61
Yann Hourdel <yann@hourdel.fr>
2019-11-28 12:02:15 +0100

    Fix migration

--- db/migrate/20190706135146_create_missing_initial_versions.rb
+++ db/migrate/20190706135146_create_missing_initial_versions.rb
@@ -1,16 +1,17 @@
 class CreateMissingInitialVersions < ActiveRecord::Migration[6.0]
   def change
-    admin_user = AdminUser.first
-    default_whodunnit = [admin_user.id, admin_user.email].join(':')
+    if admin_user = AdminUser.first
+      default_whodunnit = [admin_user.id, admin_user.email].join(':')
 
-    [
-      Child,
-      Parent
-    ].each do |klass|
-      klass.find_each do |record|
-        if record.versions.empty?
-          PaperTrail.request.whodunnit = default_whodunnit
-          record.paper_trail.save_with_version
+      [
+        Child,
+        Parent
+      ].each do |klass|
+        klass.find_each do |record|
+          if record.versions.empty?
+            PaperTrail.request.whodunnit = default_whodunnit
+            record.paper_trail.save_with_version
+          end
         end
       end
     end
