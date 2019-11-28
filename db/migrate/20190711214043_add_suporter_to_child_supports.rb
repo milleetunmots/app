7e4596389ccf9fe47f0b3dfcb39fd93882761e3d
Yann Hourdel <yann@hourdel.fr>
2019-11-28 12:13:40 +0100

    Fix migration again

--- db/migrate/20190711214043_add_suporter_to_child_supports.rb
+++ db/migrate/20190711214043_add_suporter_to_child_supports.rb
@@ -1,6 +1,5 @@
 class AddSuporterToChildSupports < ActiveRecord::Migration[6.0]
   def change
     add_reference :child_supports, :supporter, foreign_key: { to_table: :admin_users }
-    ChildSupport.update_all(supporter_id: AdminUser.first.id)
   end
 end
