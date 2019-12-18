32fd60ff94083d82f03cb96b11ca9744096e45d5
Yann Hourdel <yann@hourdel.fr>
2019-12-19 00:04:07 +0100

    use constant

--- app/services/export_redirection_target_stats_service.rb
+++ app/services/export_redirection_target_stats_service.rb
@@ -2,50 +2,50 @@ class ExportRedirectionTargetStatsService
 
   attr_reader :errors, :csv
 
+  CHILD_ATTRIBUTES = %i[
+    id
+
+    first_name
+    last_name
+    birthdate
+    age
+    gender
+    letterbox_name
+    address
+    city_name
+    postal_code
+
+    parent1_gender
+    parent1_first_name
+    parent1_last_name
+    parent1_phone_number_national
+    should_contact_parent1
+
+    parent2_gender
+    parent2_first_name
+    parent2_last_name
+    parent2_phone_number_national
+    should_contact_parent2
+
+    registration_source
+    registration_source_details
+
+    group_name
+    has_quit_group
+  ].freeze
+
   def initialize(redirection_target:)
     @redirection_target = redirection_target
     @errors = []
   end
 
   def call
-    attributes = %i[
-      id
-
-      first_name
-      last_name
-      birthdate
-      age
-      gender
-      letterbox_name
-      address
-      city_name
-      postal_code
-
-      parent1_gender
-      parent1_first_name
-      parent1_last_name
-      parent1_phone_number_national
-      should_contact_parent1
-
-      parent2_gender
-      parent2_first_name
-      parent2_last_name
-      parent2_phone_number_national
-      should_contact_parent2
-
-      registration_source
-      registration_source_details
-
-      group_name
-      has_quit_group
-    ]
-
     csv_options = ActiveAdmin.application.csv_options.clone
     bom = csv_options.delete :byte_order_mark
     csv_options[:headers] = true
 
     @csv = bom + CSV.generate(csv_options) do |csv|
-      csv << attributes.map do |key|
+      csv << CHILD_ATTRIBUTES.map do |key|
         I18n.t("activerecord.attributes.child.#{key}")
       end + [
         'URL cible',
@@ -59,7 +59,7 @@ class ExportRedirectionTargetStatsService
           child: child
         ).with_visits.any?
 
-        csv << attributes.map do |key|
+        csv << CHILD_ATTRIBUTES.map do |key|
           child.decorate.send(key)
         end + [
           @redirection_target.id,
