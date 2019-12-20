211f7b31d253fc5f06798f2ed6a7806dfdbda7a9
Yann Hourdel <yann@hourdel.fr>
2019-12-20 23:57:38 +0100

    Hotfix broken migration

--- db/migrate/20191215172648_add_parent_to_contact_to_children.rb
+++ db/migrate/20191215172648_add_parent_to_contact_to_children.rb
@@ -2,10 +2,5 @@ class AddParentToContactToChildren < ActiveRecord::Migration[6.0]
   def change
     add_column :children, :parent_to_contact_id, :integer
     add_index :children, :parent_to_contact_id
-
-    Child.find_each do |child|
-      # this should trigger the #define_parent_to_contact method
-      child.save!
-    end
   end
 end
