c5329e1f8d0d48768b1e102758fbc2e3a26618ea
Yann Hourdel <yann@hourdel.fr>
2020-05-04 15:08:23 +0200

    improve import

--- app/services/children_import_service.rb
+++ app/services/children_import_service.rb
@@ -9,7 +9,7 @@ class ChildrenImportService
 
   def call
     Child.transaction do
-      CSV.foreach(@csv_file.path, headers: true, col_sep: ';').with_index do |row, i|
+      CSV.foreach(@csv_file.path, headers: true, col_sep: ',').with_index do |row, i|
         line = i + 2
         # puts "line #{line}"
         # puts row.inspect
@@ -27,24 +27,46 @@ class ChildrenImportService
           registration_source_details: row['registration_source_details']&.strip || '?',
           first_name: row['first_name']&.strip,
           last_name: row['last_name']&.strip,
-          birthdate: Date.parse(row['birthdate']&.strip)
+          birthdate: Date.parse(row['birthdate']&.strip),
+          group_id: row['group_id']&.strip
         }
 
-        # parent 1
+        # fecth a few parent attributes
+        parent1_gender = row['parent1_gender']&.strip == 'Maman' ? 'f' : 'm'
         parent1_phone_number = format_phone_number(row['parent1_phone_number']&.strip)
         parent1_first_name = row['parent1_first_name']&.strip
         parent1_last_name = row['parent1_last_name']&.strip
+        parent2_gender = row['parent2_gender'] && (row['parent2_gender']&.strip == 'Maman' ? 'f' : 'm')
+        parent2_phone_number = format_phone_number(row['parent2_phone_number']&.strip)
+        parent2_first_name = row['parent2_first_name']&.strip
+        parent2_last_name = row['parent2_last_name']&.strip
+
+        # allow contacts if given
+        unless parent1_phone_number.blank?
+          attributes[:should_contact_parent1] = true
+        end
+        unless parent2_phone_number.blank?
+          attributes[:should_contact_parent2] = true
+        end
+
+        # link to existing parents ?
         existing_parent1 = Parent.where(phone_number: parent1_phone_number).order(:id).first
         if existing_parent1
           attributes[:parent1_id] = existing_parent1.id
           attributes[:parent2_id] = existing_parent1.children.order(:id).last.parent2_id
         else
+
+          # new parent 1
+          use_default_letterbox_name = row['parent1_letterbox_name']&.strip.blank?
+
           attributes[:parent1_attributes] = {
-            gender: row['parent1_gender']&.strip == 'Maman' ? 'f' : 'm',
+            gender: parent1_gender,
             first_name: parent1_first_name,
             last_name: parent1_last_name,
             phone_number: parent1_phone_number,
-            letterbox_name: row['parent1_letterbox_name']&.strip || [parent1_first_name, parent1_last_name].compact.join(' '),
+            letterbox_name: (
+              use_default_letterbox_name ? [parent1_first_name, parent1_last_name].compact.join(' ') : row['parent1_letterbox_name']&.strip
+            ),
             address: row['parent1_address']&.strip,
             city_name: row['parent1_city_name']&.strip,
             postal_code: row['parent1_postal_code']&.strip,
@@ -52,13 +74,20 @@ class ChildrenImportService
           }
 
           # parent 2
-          parent2_gender = row['parent2_gender'] && (row['parent2_gender']&.strip == 'Maman' ? 'f' : 'm')
           if parent2_gender
+            if use_default_letterbox_name
+              attributes[:parent1_attributes][:letterbox_name]  = [
+                attributes[:parent1_attributes][:letterbox_name],
+                parent2_first_name,
+                parent2_last_name
+              ].compact.join(' ')
+            end
+
             attributes[:parent2_attributes] = {
               gender: parent2_gender,
-              first_name: row['parent2_first_name']&.strip,
-              last_name: row['parent2_last_name']&.strip,
-              phone_number: format_phone_number(row['parent2_phone_number']&.strip) || parent1_phone_number,
+              first_name: parent2_first_name,
+              last_name: parent2_last_name,
+              phone_number: parent2_phone_number || parent1_phone_number,
               letterbox_name: attributes[:parent1_attributes][:letterbox_name],
               address: attributes[:parent1_attributes][:address],
               city_name: attributes[:parent1_attributes][:city_name],
