0038d075d8798bb69e385e73922cb6c2a6c9b8ad
Yann Hourdel <yann@hourdel.fr>
2020-08-26 19:20:22 +0200

    create task on redraft

--- app/admin/media/text_messages_bundle.rb
+++ app/admin/media/text_messages_bundle.rb
@@ -7,6 +7,7 @@ ActiveAdmin.register Media::TextMessagesBundle do
   has_better_csv
   has_paper_trail
   has_tags
+  has_tasks
   use_discard
 
   register_text_messages_bundle_index
@@ -17,20 +18,33 @@ ActiveAdmin.register Media::TextMessagesBundle do
   # ACTIONS
   # ---------------------------------------------------------------------------
 
-  action_item :draft, only: :show do
+  action_item :redraft, only: :show do
     link_to 'Remettre en brouillon',
-            [:draft, :admin, resource],
-            method: :put,
+            [:redraft, :admin, resource],
             class: 'drafts-link red'
   end
 
+  member_action :redraft do
+    @task = Task.new
+    render 'admin/media/text_messages_bundles/redraft'
+  end
+
   member_action :draft, method: :put do
-    if resource.draft
-      redirect_to admin_media_text_messages_bundle_draft_path(resource),
-                  notice: 'Remis en brouillon'
-    else
-      flash[:error] = 'Impossible à remettre en brouillon'
-      redirect_to request.referer
+    @task = Task.new
+    task_params = params.require(:task).permit(:title, :description, :due_date, :assignee_id)
+    if task_params[:title]
+      @task.attributes = task_params
+      @task.related = resource
+      @task.reporter = current_admin_user
+    end
+    resource.class.transaction do
+      if (@task.title.blank? or @task.save) && resource.draft
+        redirect_to admin_media_text_messages_bundle_draft_path(resource),
+                    notice: 'Remis en brouillon'
+      else
+        flash[:error] = 'Impossible à remettre en brouillon'
+        render 'admin/media/text_messages_bundles/redraft'
+      end
     end
   end
 
