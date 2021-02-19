c8b81b012cf1a1a7277ec1f5db41b2e962a1b744
Yann Hourdel <yann@hourdel.fr>
2021-02-19 15:42:52 +0100

    improve modules selection on additional module too

--- app/helpers/active_admin/support_module_weeks_helper.rb
+++ app/helpers/active_admin/support_module_weeks_helper.rb
@@ -1,7 +1,8 @@
 module ActiveAdmin::SupportModuleWeeksHelper
 
   def support_module_week_medium_select_collection
-    Media::TextMessagesBundle.order(:name).map do |msg|
+    Media::TextMessagesBundle.order(:name)
+                             .map do |msg|
       [
         msg.decorate.select_collection_option_label,
         msg.id
@@ -10,7 +11,14 @@ module ActiveAdmin::SupportModuleWeeksHelper
   end
 
   def support_module_week_additional_medium_select_collection
-    Media::TextMessagesBundle.single_message.order(:name).map(&:decorate)
+    Media::TextMessagesBundle.single_message
+                             .order(:name)
+                             .map do |msg|
+      [
+        msg.decorate.select_collection_option_label,
+        msg.id
+      ]
+    end
   end
 
 end
