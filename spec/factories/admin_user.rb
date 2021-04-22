2991541f0f5f84d720b859111da704e0480e4309
Aristide Bamenou <aristide.bamenou@hotmail.com>
2021-04-22 13:11:27 +0200

    Redirection url spec file

--- spec/factories/admin_user.rb
+++ spec/factories/admin_user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :admin_user do
-    name    { Faker::Name.name }
-    email { Faker::Internet.email }
-    password { Faker::Internet.password }
+    name      { Faker::Name.name }
+    email     { Faker::Internet.email }
+    password  { Faker::Internet.password }
   end
 end
