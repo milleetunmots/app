df5adfcb583acd302c6928a8c91ebeb9f268c34c
Aristide Bamenou <aristide.bamenou@hotmail.com>
2021-04-29 17:50:45 +0200

    scopes test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -23,6 +23,13 @@
 require "rails_helper"
 
 RSpec.describe Event, type: :model do
+  before(:each) do
+    @other_event = FactoryBot.create(:other_event)
+    @survey_response = FactoryBot.create(:survey_response)
+    @text_message = FactoryBot.create(:text_message)
+    @workshop_participation = FactoryBot.create(:workshop_participation)
+  end
+
   describe "Validations" do
     context "succeed" do
       it "if event has been occurred" do
@@ -33,6 +40,38 @@ RSpec.describe Event, type: :model do
     context "fail" do
       it "if event hasn't been occurred" do
         expect(FactoryBot.build_stubbed(:event, occurred_at: nil)).not_to be_valid
+      end
+    end
+  end
+
+  describe "#other_events" do
+    context "returns" do
+      it "other events" do
+        expect(Event.other_events).to match_array [@other_event]
+      end
+    end
+  end
+
+  describe "#survey_reponses" do
+    context "returns" do
+      it "survey responses" do
+        expect(Event.survey_responses).to match_array [@survey_response]
+      end
+    end
+  end
+
+  describe "#text_messages" do
+    context "returns" do
+      it "text messages" do
+        expect(Event.text_messages).to match_array [@text_message]
+      end
+    end
+  end
+
+  describe "#workshop_participations" do
+    context "returns" do
+      it "workshops" do
+        expect(Event.workshop_participations).to match_array [@workshop_participation]
       end
     end
   end
