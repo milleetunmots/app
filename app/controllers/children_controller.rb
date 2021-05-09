d1cf932a3eb5a51e309eea3a845277cb6dd1f4a9
Aristide Bamenou <aristide.bamenou@hotmail.com>
2021-05-09 13:30:21 +0200

    simplifying this complex logical expression

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -70,7 +70,8 @@ class ChildrenController < ApplicationController
     mother_attributes_valid = !mother_attributes[:first_name].blank? && !mother_attributes[:last_name].blank? && !mother_attributes[:phone_number].blank?
     father_attributes_available = !father_attributes[:first_name].blank? || !father_attributes[:last_name].blank? || !father_attributes[:phone_number].blank?
     father_attributes_valid = !father_attributes[:first_name].blank? && !father_attributes[:last_name].blank? && !father_attributes[:phone_number].blank?
-    if (mother_attributes_available && !mother_attributes_valid) || (father_attributes_available && !father_attributes_valid) || (!mother_attributes_available && !father_attributes_available)
+    creation = (mother_attributes_available && !mother_attributes_valid) || (father_attributes_available && !father_attributes_valid) || (!mother_attributes_available && !father_attributes_available)
+    if creation
       flash.now[:error] = "Inscription refusée"
       @child = Child.new(attributes.merge(
         parent1_attributes: parent1_attributes.merge(mother_attributes),
