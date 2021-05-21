0f5f8c323eb4d22dbb5f01d695cee2c28224b709
Aristide Bamenou <aristide.bamenou@hotmail.com>
2021-05-21 15:43:43 +0200

    Suppression du helper avec AGES

--- app/helpers/active_admin/support_modules_helper.rb
+++ app/helpers/active_admin/support_modules_helper.rb
@@ -1,12 +1,2 @@
 module ActiveAdmin::SupportModulesHelper
-
-  def support_module_ages_select_collection
-    SupportModule::AGES.map do |v|
-      [
-        SupportModule.human_attribute_name("ages.#{v}"),
-        v
-      ]
-    end
-  end
-
 end
