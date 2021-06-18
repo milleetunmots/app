f287283b3189b36388be21c7a5674a8bc27639f8
Aristide Bamenou <aristide.bamenou@hotmail.com>
2021-06-18 15:21:46 +0200

    Ability reformat

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,18 +4,22 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user.present?
-      can :manage, [Child, Task]
-      if user.admin?
-        can :manage, :all
-      elsif user.team_member?
-        can :manage, [Medium, Tag, Event]
-        can [:read, :update], [Group, Parent]
-        can :read, [RedirectionUrl, ChildSupport]
-      else
-        can :manage, [Parent, ChildSupport]
-        can :read, [Group, Medium, RedirectionUrl, Tag, Event]
-      end
+    return unless user.present?
+    if user.admin?
+      can :manage, :all
+      return
+    end
+
+    can :manage, [Child, Task]
+    can :read, ActiveAdmin::Page, name: 'Dashboard'
+
+    if user.team_member?
+      can :manage, [Medium, Tag, Event]
+      can [:read, :update], [Group, Parent]
+      can :read, [RedirectionUrl, ChildSupport]
+    else
+      can :manage, [Parent, ChildSupport]
+      can :read, [Group, Medium, RedirectionUrl, Tag, Event]
     end
   end
 end
