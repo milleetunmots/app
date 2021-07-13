60516dbbbec6a0fd735ede17264b95380cde6701
Aristide Bamenou <aristide.bamenou@1001mots.org>
2021-07-13 10:29:01 +0200

    Migration fixed

--- db/migrate/20210712125915_change_call1_books_quantity_type_for_child_support.rb
+++ db/migrate/20210712125915_change_call1_books_quantity_type_for_child_support.rb
@@ -1,5 +1,9 @@
 class ChangeCall1BooksQuantityTypeForChildSupport < ActiveRecord::Migration[6.0]
-  def change
-    change_column :child_supports, :call1_books_quantity, :string
+  def up
+    change_column :child_supports, :call1_books_quantity, :string, using: 'call1_books_quantity::integer'
+  end
+
+  def down
+    change_column :child_supports, :call1_books_quantity, :integer
   end
 end
