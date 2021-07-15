e04ca8af3cb181dbad243305245469941cde4f39
Aristide Bamenou <aristide.bamenou@1001mots.org>
2021-07-15 18:43:56 +0200

    Migration fixed "="

--- db/migrate/20210713143428_add_temp_to_child_support.rb
+++ db/migrate/20210713143428_add_temp_to_child_support.rb
@@ -4,6 +4,6 @@ class AddTempToChildSupport < ActiveRecord::Migration[6.0]
     ChildSupport.where("call1_books_quantity > 10").update_all(temp: "4_more_than_ten")
     ChildSupport.where("call1_books_quantity > 5 AND call1_books_quantity <= 10").update_all(temp: "3_five_to_ten")
     ChildSupport.where("call1_books_quantity > 0 AND call1_books_quantity <= 5").update_all(temp: "2_one_to_five")
-    ChildSupport.where("call1_books_quantity == 0").update_all(temp: "1_none")
+    ChildSupport.where("call1_books_quantity = 0").update_all(temp: "1_none")
   end
 end
