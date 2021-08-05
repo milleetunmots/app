9a0f6d32d3650182ea8747a964095d535276f2cb
Aristide Bamenou <aristide.bamenou@hotmail.com>
2021-08-05 14:59:57 +0200

    migration fixed

--- db/migrate/20210805105530_add_social_networks_to_child_support.rb
+++ db/migrate/20210805105530_add_social_networks_to_child_support.rb
@@ -1,7 +1,7 @@
 class AddSocialNetworksToChildSupport < ActiveRecord::Migration[6.0]
   def change
-    add_column :child_supports, :facebook, :boolean, null: true, default: null
-    add_column :child_supports, :whatsapp, :boolean, null: true, default: null
-    add_column :child_supports, :instagram, :boolean, null: true, default: null
+    add_column :child_supports, :facebook, :boolean, null: true, default: nil
+    add_column :child_supports, :whatsapp, :boolean, null: true, default: nil
+    add_column :child_supports, :instagram, :boolean, null: true, default: nil
   end
 end
