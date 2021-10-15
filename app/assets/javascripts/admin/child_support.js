3615adcddde927ddc9ea05220e0935b134d20502
aristidebamenou <aristide.bamenou@hotmail.com>
2021-10-15 16:29:02 +0200

    autocompletion's function

--- app/assets/javascripts/admin/child_support.js
+++ app/assets/javascripts/admin/child_support.js
@@ -2,6 +2,18 @@ $(document).ready(function() {
   let parent1 = $('#child_parent1_id');
   let parent2 = $('#child_parent2_id');
 
+  let autocompletion = function(input, id) {
+    input.change(function() {
+      $.getJSON(`/parent/${$(id).val()}/first_child`, function(child) {
+        $('#child_registration_source').val(child.registration_source).change();
+        $('#child_registration_source_details').val(child.registration_source_details);
+        $('#child_group_id').val(child.group_id).change();
+        addTags(["fratrie ajoutée"])
+      });
+    })
+
+  }
+
   let addTags = function(tag_list) {
     tag_list.forEach((tag)=>{
       let $foundOption = $('#child_tag_list').find("option[value='" + tag + "']")
@@ -15,22 +27,7 @@ $(document).ready(function() {
     })
   }
 
-  parent1.change(function() {
-    $.getJSON(`/parent/${$('#child_parent1_id').val()}/first_child`, function(child) {
-      $('#child_registration_source').val(child.registration_source).change();
-      $('#child_registration_source_details').val(child.registration_source_details);
-      $('#child_group_id').val(child.group_id).change();
-      addTags(["fratrie ajoutée"])
-    });
-  });
-
-  parent2.change(function() {
-    $.getJSON(`/parent/${$('#child_parent2_id').val()}/first_child`, function(child) {
-      $('#child_registration_source').val(child.registration_source).change();
-      $('#child_registration_source_details').val(child.registration_source_details);
-      $('#child_group_id').val(child.group_id).change();
-      addTags(["fratrie ajoutée"])
-    });
-  });
+  autocompletion(parent1, '#child_parent1_id');
+  autocompletion(parent2, '#child_parent2_id');
 });
 
