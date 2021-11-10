effb15ac43f7aa51946d007cbc1a3dc1cbe317a8
aristidebamenou <aristide.bamenou@hotmail.com>
2021-11-10 17:27:33 +0100

    migration

--- db/migrate/20211108155941_reformat_child_support_call_status.rb
+++ db/migrate/20211108155941_reformat_child_support_call_status.rb
@@ -1,12 +1,8 @@
 class ReformatChildSupportCallStatus < ActiveRecord::Migration[6.0]
   def change
     (1..5).each do |call_idx|
-      ChildSupport.where("unaccent(call#{call_idx}_status) ILIKE unaccent('%OK%')").each do |child_support|
-        child_support.update_column "call#{call_idx}_status".to_sym, "OK"
-      end
-      ChildSupport.where("unaccent(call#{call_idx}_status) ILIKE unaccent('%KO%')").each do |child_support|
-        child_support.update_column "call#{call_idx}_status".to_sym, "KO"
-      end
+      ChildSupport.where("unaccent(call#{call_idx}_status) ILIKE unaccent('%OK%')").update_all("call#{call_idx}_status" => "OK")
+      ChildSupport.where("unaccent(call#{call_idx}_status) ILIKE unaccent('%KO%')").update_all("call#{call_idx}_status" => "KO")
     end
   end
 end
