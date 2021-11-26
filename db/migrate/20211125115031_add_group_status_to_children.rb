d1acd1993ccdda2a604c17a57ae4abd6fc559af7
aristidebamenou <aristide.bamenou@hotmail.com>
2021-11-26 16:59:01 +0100

    Migration fixed

--- db/migrate/20211125115031_add_group_status_to_children.rb
+++ db/migrate/20211125115031_add_group_status_to_children.rb
@@ -3,19 +3,5 @@ class AddGroupStatusToChildren < ActiveRecord::Migration[6.0]
     add_column :children, :group_status, :string, default: "waiting"
     add_column :children, :group_start, :date
     add_column :children, :group_end, :date
-
-    Child.reset_column_information
-
-    Child.where.not(group_id: nil).each do |child|
-      group = Group.find(child.group_id)
-      child.update group_start: group.started_at
-      if group.ended_at.past?
-        child.update group_end: group.ended_at, group_status: "stopped"
-      else
-        child.update group_status: child.has_quit_group ? "paused" : "active"
-      end
-    end
-
-    remove_column :children, :has_quit_group
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_115031) do
+ActiveRecord::Schema.define(version: 2021_11_26_155539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
