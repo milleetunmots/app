e8b1dbf29608b00047faba4af7a5a340e8fbe0d0
aristidebamenou <aristide.bamenou@hotmail.com>
2021-12-01 20:40:22 +0100

    clean up migration

--- app/decorators/workshop_decorator.rb
+++
@@ -1,6 +0,0 @@
-class WorkshopDecorator < BaseDecorator
-
-  def workshop_address
-    "#{address} #{postal_code} #{city_name}"
-  end
-end
--- db/migrate/20211130235017_add_workshop_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddWorkshopToEvents < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :events, :workshop, foreign_key: true
-  end
-end
