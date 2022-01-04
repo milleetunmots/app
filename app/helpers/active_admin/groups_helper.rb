31de71ea053510ba84b3e1bf61c70ef5f890f7dc
aristidebamenou <aristide.bamenou@hotmail.com>
2022-01-04 17:13:27 +0100

    groups_data requests

--- app/helpers/active_admin/groups_helper.rb
+++ app/helpers/active_admin/groups_helper.rb
@@ -17,23 +17,18 @@ module ActiveAdmin::GroupsHelper
     group_ids = Group.where(name: groups).pluck(:id)
     support_ids = ChildSupport.where(call3_sendings_benefits: call3_sending_benefits).pluck(:id)
 
-    children = Child.where(
-      created_at: (registration_start..registration_end),
-      registration_source: registration_sources,
-      land: lands
-    )
+    children = Child.where(created_at: (registration_start..registration_end))
+      .registration_months_between(age_start.gsub(" mois", "").to_i, age_end.gsub(" mois", "").to_i)
     children = children.where(group_id: group_ids) if groups
+    children = children.where(land: lands) if lands
     children = children.where(child_support_id: support_ids) if call3_sending_benefits
-
-    children = children.tagged_with(tags) if tags
-
-    children = children.select { |child| child.registration_months <= age_end.gsub(" mois", "").to_i && child.registration_months >= age_start.gsub(" mois", "").to_i }
-
+    children = children.where(registration_source: registration_sources) if registration_sources
+    children = children.tagged_with(tags, any: true) if tags
 
     values["goal"] = 4000
     values["active_groups_count"] = Group.where("started_at < ?", DateTime.now).where("ended_at > ?", DateTime.now).count
     values["stopped_groups_count"] = Group.where("ended_at < ?", DateTime.now).count
-    values["families_count"] = children.map { |item| item.parent1_id }.uniq.count
+    values["families_count"] = children.families_count
     values["children_count"] = children.count
 
     values
