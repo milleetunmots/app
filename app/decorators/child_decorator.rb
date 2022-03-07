c46a86463752837b5a5b1868b83fd036bfe449c6
aristidebamenou <aristide.bamenou@hotmail.com>
2022-03-07 10:19:32 +0100

    fix: registration_months_range nil unless registration_months

--- app/decorators/child_decorator.rb
+++ app/decorators/child_decorator.rb
@@ -193,6 +193,8 @@ class ChildDecorator < BaseDecorator
   end
 
   def registration_months_range
+    return nil unless registration_months
+
     if registration_months >= 36
       "Plus de 36 mois"
     elsif registration_months >= 24
