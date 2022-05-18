f00970269920a60f4e9b22d0ae85d85da62ea6cd
aristidebamenou <aristide.bamenou@hotmail.com>
2022-05-18 15:50:53 +0200

    dashboard programme à distance

---
+++ app/helpers/active_admin/remotes_helper.rb
@@ -0,0 +1,39 @@
+module ActiveAdmin::RemotesHelper
+
+  def remote_data_count(
+    date_start, date_end,
+    age_start, age_end,
+    groups,
+    lands,
+    call3_sending_benefits,
+    registration_sources,
+    tags
+  )
+    values = {}
+
+    group_ids = Group.where(name: groups).pluck(:id)
+    support_ids = ChildSupport.where(call3_sendings_benefits: call3_sending_benefits).pluck(:id)
+
+    children = Child.where(created_at: (date_start..date_end))
+                    .registration_months_between(age_start.gsub(" mois", "").to_i, age_end.gsub(" mois", "").to_i)
+    children = children.where(group_id: group_ids) if groups
+    children = children.where(land: lands) if lands
+    children = children.where(child_support_id: support_ids) if call3_sending_benefits
+    children = children.where(registration_source: registration_sources) if registration_sources
+    children = children.tagged_with(tags, any: true) if tags
+
+    parent_ids = (children.joins(:family).pluck(:parent1_id) + children.joins(:family).pluck(:parent2_id)).compact
+
+    byebug
+
+    values["messages_received"] = Event.where(related_type: "Parent", related_id: parent_ids).text_messages_send_by_app.count / parent_ids.length unless parent_ids.length.zero?
+    values["messages_sent"] = Event.where(related_type: "Parent", related_id: parent_ids).text_messages_send_by_parent.count / parent_ids.length unless parent_ids.length.zero?
+    # values["active_groups"] = Group.where("started_at < ?", DateTime.now).where("ended_at > ?", DateTime.now).count
+    # values["stopped_groups"] = Group.where("ended_at < ?", DateTime.now).count
+    # values["children_followed"] = children.where(group_status: "active").count
+    # values["families_followed"] = children.where(group_status: "active").families_count
+
+    values
+  end
+
+end
