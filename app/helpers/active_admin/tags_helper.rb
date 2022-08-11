52cd19d0596b5600caab7842e2740a1ff0dc73fd
GRENOUILLETxavier <xavier.grenouillet@leikir.io>
2022-08-11 19:22:13 +0200

    integration: initial form data into ChildSupport#edit

--- app/helpers/active_admin/tags_helper.rb
+++ app/helpers/active_admin/tags_helper.rb
@@ -4,20 +4,22 @@ module ActiveAdmin::TagsHelper
     ActsAsTaggableOn::Tag.order("LOWER(name)").pluck(:name)
   end
 
-  def tags_input(form, options = {})
+  def tags_input(form, options = {}, width: nil)
+    input_html = {
+      data: {
+        select2: {
+          tags: true,
+          tokenSeparators: [","]
+        }
+      }
+    }
+    input_html[:width] = width if width
     form.input :tag_list,
       {
         multiple: true,
         label: "Tags",
         collection: tag_name_collection,
-        input_html: {
-          data: {
-            select2: {
-              tags: true,
-              tokenSeparators: [","]
-            }
-          }
-        }
+        input_html: input_html
       }.deep_merge(options)
   end
 
