c3e84ff299403d3665a0a3d1df2fc1a3aa3536cb
Yann Hourdel <yann@hourdel.fr>
2022-08-12 15:27:52 +0200

    fix migrations again

--- db/migrate/20220623091513_fill_parents_networks.rb
+++ db/migrate/20220623091513_fill_parents_networks.rb
@@ -1,19 +1,19 @@
 class FillParentsNetworks < ActiveRecord::Migration[6.0]
   def up
     ChildSupport.all.each do |child_support|
-      child_support.parent1.update! follow_us_on_facebook: true if child_support.follow_us_on&.include?("Facebook national")
+      child_support.parent1&.update! follow_us_on_facebook: true if child_support.follow_us_on&.include?("Facebook national")
       child_support.parent2&.update! follow_us_on_facebook: true if child_support.follow_us_on&.include?("Facebook national")
 
-      child_support.parent1.update! follow_us_on_facebook: true if child_support.follow_us_on&.include?("Facebook local")
+      child_support.parent1&.update! follow_us_on_facebook: true if child_support.follow_us_on&.include?("Facebook local")
       child_support.parent2&.update! follow_us_on_facebook: true if child_support.follow_us_on&.include?("Facebook local")
 
-      child_support.parent1.update! follow_us_on_whatsapp: true if child_support.follow_us_on&.include?("WhatsApp")
+      child_support.parent1&.update! follow_us_on_whatsapp: true if child_support.follow_us_on&.include?("WhatsApp")
       child_support.parent2&.update! follow_us_on_whatsapp: true if child_support.follow_us_on&.include?("WhatsApp")
 
-      child_support.parent1.update! present_on_facebook: true if child_support.present_on&.include?("Facebook")
+      child_support.parent1&.update! present_on_facebook: true if child_support.present_on&.include?("Facebook")
       child_support.parent2&.update! present_on_facebook: true if child_support.present_on&.include?("Facebook")
 
-      child_support.parent1.update! present_on_whatsapp: true if child_support.present_on&.include?("WhatsApp")
+      child_support.parent1&.update! present_on_whatsapp: true if child_support.present_on&.include?("WhatsApp")
       child_support.parent2&.update! present_on_whatsapp: true if child_support.present_on&.include?("WhatsApp")
     end
   end
--- db/migrate/20220623093639_fill_parents_networks_with_child_support_tags.rb
+++ db/migrate/20220623093639_fill_parents_networks_with_child_support_tags.rb
@@ -1,15 +1,15 @@
 class FillParentsNetworksWithChildSupportTags < ActiveRecord::Migration[6.0]
   def change
     ChildSupport.tagged_with("facebook").each do |child_support|
-      child_support.parent1.update! present_on_facebook: true
-      child_support.parent1.update! follow_us_on_facebook: true
+      child_support.parent1&.update! present_on_facebook: true
+      child_support.parent1&.update! follow_us_on_facebook: true
       child_support.parent2&.update! present_on_facebook: true
       child_support.parent2&.update! follow_us_on_facebook: true
     end
 
     ChildSupport.tagged_with("Whatsapp").each do |child_support|
-      child_support.parent1.update! present_on_whatsapp: true
-      child_support.parent1.update! follow_us_on_whatsapp: true
+      child_support.parent1&.update! present_on_whatsapp: true
+      child_support.parent1&.update! follow_us_on_whatsapp: true
       child_support.parent2&.update! present_on_whatsapp: true
       child_support.parent2&.update! follow_us_on_whatsapp: true
     end
