68de90544b91253af6a9aa588a96e57eee611130
aristidebamenou <aristide.bamenou@hotmail.com>
2022-09-01 15:03:48 +0200

    child support fixed

--- app/services/typeform/initial_form_service.rb
+++ app/services/typeform/initial_form_service.rb
@@ -1,7 +1,7 @@
 module Typeform
   class InitialFormService
     include ApplicationHelper
-    
+
     FIELD_IDS = {
       name: ENV['TYPEFORM_NAME'],
       child_count: ENV['TYPEFORM_CHILD_COUNT'],
@@ -20,7 +20,7 @@ module Typeform
       would_like_to_do_more: ENV['TYPEFORM_WOULD_LIKE_TO_DO_MORE'],
       would_receive_advices: ENV['TYPEFORM_WOULD_LIKE_TO_RECEIVE_ADVICES'],
     }
-    
+
 
     attr_reader :data
 
@@ -39,7 +39,7 @@ module Typeform
       parse_answers
       update_parents
       update_child_support
-      
+
       self
     end
 
@@ -47,7 +47,7 @@ module Typeform
       @answers.each do |answer|
         case answer[:field][:id]
         when FIELD_IDS[:name]
-          @data[:name] = answer[:text] 
+          @data[:name] = answer[:text]
         when FIELD_IDS[:child_count]
           @data[:child_count] = answer[:choice][:label]
         when FIELD_IDS[:already_working_with]
@@ -66,11 +66,11 @@ module Typeform
         when FIELD_IDS[:most_present_parent]
           case answer[:choice][:label]
           when 'Moi'
-            @data[:most_present_parent] = "#{@child_support.parent1.first_name} #{@child_support.parent1.last_name}" 
+            @data[:most_present_parent] = "#{@child_support.parent1.first_name} #{@child_support.parent1.last_name}"
           when "Plutôt l'autre parent"
             @data[:most_present_parent] = @child_support.parent2 ? "#{@child_support.parent2.first_name child_support.parent2.last_name}" : "L'autre parent"
           when 'Les deux pareil !'
-            @data[:most_present_parent] = 'Les deux parents' 
+            @data[:most_present_parent] = 'Les deux parents'
           else
             @data[:most_present_parent] = answer[:choice][:label]
           end
@@ -132,12 +132,18 @@ module Typeform
       informations << @child_support.important_information if @child_support.important_information
       informations << "Nombre d'enfants: #{@data[:child_count]}" if @data[:child_count]
       informations << "À déjà été accompagné par 1001 mots" if @data[:already_working_with]
-      informations << "#{@data[:most_present_parent]} passe le plus de temps avec l'enfant" if @data[:most_present_parent]
+      if @data[:most_present_parent]
+        if @data[:most_present_parent] == 'Les deux parents'
+          informations << "#{@data[:most_present_parent]} passent le plus de temps avec l'enfant"
+        else
+          informations << "#{@data[:most_present_parent]} passe le plus de temps avec l'enfant"
+        end
+      end
       @child_support.important_information = informations.join("\n")
 
-      
 
-      
+
+
       unless @data[:other_parent_phone] == @parent1.phone_number || @data[:other_parent_phone] == @parent2&.phone_number
         @child_support.other_phone_number = @data[:other_parent_phone]
         @child_support.important_information += "\n#{@data[:other_parent_phone]}"
