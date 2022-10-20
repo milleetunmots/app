fa55d4def66fca5bf453e95c8c7ed547b16623a7
aristidebamenou <aristide.bamenou@hotmail.com>
2022-10-20 12:50:12 +0200

    feature: spot_hit_message_id added to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_11_102418) do
+ActiveRecord::Schema.define(version: 2022_10_20_104625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -220,6 +220,7 @@ ActiveRecord::Schema.define(version: 2022_10_11_102418) do
     t.boolean "originated_by_app", default: true, null: false
     t.bigint "workshop_id"
     t.string "parent_response"
+    t.string "spot_hit_campaign_id"
     t.index ["discarded_at"], name: "index_events_on_discarded_at"
     t.index ["related_type", "related_id"], name: "index_events_on_related_type_and_related_id"
     t.index ["type"], name: "index_events_on_type"
