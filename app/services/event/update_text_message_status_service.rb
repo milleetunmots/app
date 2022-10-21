acf051e3e12620f4284c93c419560538493dd108
aristidebamenou <aristide.bamenou@hotmail.com>
2022-10-21 11:49:45 +0200

    feature: Updtate text message status service

--- app/services/event/update_text_message_status_service.rb
+++ app/services/event/update_text_message_status_service.rb
@@ -10,28 +10,36 @@ class Event::UpdateTextMessageStatusService
     if is_campaign_id
       update_all_text_messages_of_campaign(@message_id_from_spot_hit)
     else
-      update_text_message(@message_id_from_spot_hit)
+      update_text_message(@message_id_from_spot_hit, @status)
     end
   end
 
   private
 
   def check_message_id_from_spot_hit(id)
-
+    Events::TextMessage.where(spot_hit_message_id: id).count > 1
   end
 
-  def update_all_text_messages_of_campaign(campagn_id)
-    # uri = URI("https://www.spot-hit.fr/api/dlr")
-    # form = {
-    #   "key" => ENV["SPOT_HIT_API_KEY"],
-    #   "id" => @response["id"]
-    # }
-    # @receipts = HTTP.post(uri, form: form)
-    # @receipts = JSON.parse(@receipts.body.to_s)
+  def update_all_text_messages_of_campaign(campaign_id)
+    uri = URI("https://www.spot-hit.fr/api/dlr")
+    form = {
+      "key" => ENV["SPOT_HIT_API_KEY"],
+      "id" => campaign_id
+    }
+    @receipts = HTTP.post(uri, form: form)
+    @receipts = JSON.parse(@receipts.body.to_s)
+
+    result = @receipts.map { |receipt| {phone_number: receipt[0], status: receipt[1] } }
+
+    Events::TextMessage.where(spot_hit_message_id: campaign_id).each do |message|
+      receipt = result.find { |item| item[:phone_number] == message.related.phone_number }
+
+      receipt.nil? ? message.update(spot_hit_status: receipt[1]) : message.update!(spot_hit_status: 4)
+    end
   end
 
-  def update_text_message(message_id)
-    Events::TextMessage.find_by(spot_hit_message_id: @message_id_from_spot_hit).update!(spot_hit_status: @status)
+  def update_text_message(message_id, status)
+    Events::TextMessage.find_by(spot_hit_message_id: message_id).update!(spot_hit_status: status)
   end
 
 end
