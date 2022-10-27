c54df394b49ccee23300f23c7a9f9e19cbd1fc7d
aristidebamenou <aristide.bamenou@hotmail.com>
2022-10-27 12:04:39 +0200

    fix: Précision des erreurs

--- app/services/program_message_service.rb
+++ app/services/program_message_service.rb
@@ -124,7 +124,9 @@ class ProgramMessageService
   end
 
   def check_all_fields_are_present
-    @errors << "Tous les champs doivent être complétés." if !@planned_timestamp.present? || @recipients.empty? || @message.empty?
+    @errors << "La date n'est pas complétées." unless @planned_timestamp.present?
+    @errors << "Les destinataires ne sont pas complétés." if @recipients.empty?
+    @errors << "Le message n'est pas complétés." if @message.empty?
   end
 
   def sort_recipients
