a54d8b06d9eaf1685850a430eecacca2139bbf4a
aristidebamenou <aristide.bamenou@hotmail.com>
2022-11-17 11:02:03 +0100

    fix: resubscribing restaured

--- db/migrate/20221116091250_update_children_registration_sources.rb
+++
@@ -1,5 +0,0 @@
-class UpdateChildrenRegistrationSources < ActiveRecord::Migration[6.0]
-  def change
-    Child.where(registration_source: "resubscribing").update_all(registration_source: "other")
-  end
-end
