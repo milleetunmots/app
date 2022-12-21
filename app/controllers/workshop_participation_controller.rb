6ef516fb04c33f432b01d6a12952f825210dd3fb
aristidebamenou <aristide.bamenou@hotmail.com>
2022-12-21 09:40:29 +0100

    fix: Ajout de code de securité dans le lien des ateliers

--- app/controllers/workshop_participation_controller.rb
+++ app/controllers/workshop_participation_controller.rb
@@ -1,11 +1,6 @@
 class WorkshopParticipationController < ApplicationController
 
-  before_action do
-    @workshop_participation = Event.workshop_participations.where(
-      related_id: params[:parent_id],
-      workshop_id: params[:workshop_id]
-    ).first
-  end
+  before_action :find_workshop_participation, only: %i[edit update]
 
   def edit
     @workshop_participation_action_path = update_workshop_participation_path(
@@ -15,11 +10,26 @@ class WorkshopParticipationController < ApplicationController
   end
 
   def update
-    @workshop_participation.attributes = params.require(:workshop_participation).permit(:parent_response)
-    if @workshop_participation.save(validate: false)
+    if @workshop_participation.update(workshop_participation_params)
+      @workshop_participation.update(acceptation_date: Date.today) if @workshop_participation.parent_response == "Oui"
       redirect_to updated_workshop_participation_path
     else
       render action: :edit
     end
+  end
+
+  private
+
+  def workshop_participation_params
+    params.require(:workshop_participation).permit(:parent_response)
+  end
+
+  def find_workshop_participation
+    @workshop_participation = Event.workshop_participations.find_by(
+      related_id: params[:parent_id],
+      workshop_id: params[:workshop_id]
+    )
+    not_found and return if @workshop_participation.nil?
+    not_found and return if @workshop_participation.related.security_code != params[:parent_security_code]
   end
 end
