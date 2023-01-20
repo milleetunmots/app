52014ad3fa984fcaa434fd4eb96284693c7f5f3f
Fabien <fabien.karquel@gmail.com>
2023-01-20 11:14:04 +0100

    [WIP] feat: Send reminder to parents if they didnot fill the form (#372)
    * feat: Send reminder to parents if they didnot fill the form
    
    * feat: add sidekiq web interface
    
    * fix selectModuleService: check if module is present instead of is_completed to true
    
    Co-authored-by: Fabien Karquel <fabien@karquel.com>

---
+++ app/jobs/children_support_module/check_to_send_reminder.rb
@@ -0,0 +1,12 @@
+class ChildrenSupportModule
+  class CheckToSendReminder < ApplicationJob
+    def perform(children_support_module_id)
+      children_support_module = ChildrenSupportModule.find(children_support_module_id)
+
+      unless children_support_module.support_module.present?
+        service = ChildrenSupportModule::SendReminder.new(children_support_module).call
+        raise service.errors if service.errors.any?
+      end
+    end
+  end
+end
