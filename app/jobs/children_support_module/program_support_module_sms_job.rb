8345131d83d973b64f5c32a2fb187fb7a3a4c42e
Fabien <fabien.karquel@gmail.com>
2023-02-08 14:45:23 +0100

    Feature/check spot hit credits (#451)
    * check spot-hit credits wip
    
    * feat: count bunber of sms / mms
    
    * create task for logistic team when there is not enough spothit credits
    
    ---------
    
    Co-authored-by: Fabien Karquel <fabien@karquel.com>

--- app/jobs/children_support_module/program_support_module_sms_job.rb
+++ app/jobs/children_support_module/program_support_module_sms_job.rb
@@ -5,9 +5,11 @@ class ChildrenSupportModule
       group = Group.find(group_id)
       children_support_module_ids = ChildrenSupportModule.where(child_id: group.children.where(group_status: "active").ids)
 
+      check_service = ChildrenSupportModule::CheckCreditsService.new(children_support_module_ids).call
+      raise check_service.errors.to_json if check_service.errors.any?
+
       service = ChildSupport::ProgramChosenModulesService.new(children_support_module_ids, first_message_date).call
       errors[group.id] = service.errors if service.errors.any?
-
 
       raise errors.to_json if errors.any?
     end
