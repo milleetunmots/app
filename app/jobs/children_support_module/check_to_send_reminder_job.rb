92f4f8b62da211497be9ee82cc20ebd4647ea431
Fabien Karquel <fabien@karquel.com>
2023-02-10 17:42:19 +0100

    fix programation of modules

--- app/jobs/children_support_module/check_to_send_reminder_job.rb
+++ app/jobs/children_support_module/check_to_send_reminder_job.rb
@@ -3,7 +3,7 @@ class ChildrenSupportModule
     def perform(children_support_module_id, reminder_date)
       children_support_module = ChildrenSupportModule.find(children_support_module_id)
 
-      unless children_support_module.support_module.present?
+      unless children_support_module.support_module.present? || children_support_module.is_completed
         service = ChildrenSupportModule::SendReminder.new(children_support_module, reminder_date).call
         raise service.errors if service.errors.any?
       end
