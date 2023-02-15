2c08db7b14821c8b44cb66aca3663514e1f25746
aristidebamenou <aristide.bamenou@hotmail.com>
2023-02-15 10:47:22 +0100

    fix : Retirer le dropdown "Actions" et mettre un bouton "Modifier" à la place qui envoi directement sur la page d'edit

--- app/admin/child_support.rb
+++ app/admin/child_support.rb
@@ -31,9 +31,9 @@ ActiveAdmin.register ChildSupport do
     column :availability
     column :call_infos
     column :groups
-    actions dropdown: true do |decorated|
-      discard_links_args(decorated.model).each do |args|
-        item *args
+    column :actions do |item|
+      div class: 'table_actions' do
+        link_to('Modifier', edit_admin_child_support_path(item), class: 'edit_link member_link')
       end
     end
   end
