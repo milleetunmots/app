50b462883efe788ee73ce3a391fba5668a2db1df
Fabien Karquel <fabien@karquel.com>
2023-02-20 09:57:20 +0100

    tests: list child::create_service tests

---
+++ spec/services/child/create_service_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe Child::CreateService do
+
+  context "when params are valid" do
+    it "creates a child" do
+    end
+
+    context "when registration_origin = 3" do
+      it "adds 'form-pro' tag" do
+      end
+
+      it "sends sms at a specific time" do
+      end
+    end
+
+    context "when registration_origin = 2" do
+      it "adds 'form-2' tag" do
+      end
+
+      it "sends sms at a specific time" do
+      end
+    end
+
+    context "when registration_origin is not 2 or 3" do
+      it "adds 'form-2' site" do
+      end
+
+      it "does not send a sms" do
+      end
+    end
+
+    context "when there are 2 parents" do
+      it "sets should_contact_parent2 to true" do
+      end
+    end
+
+    context "when only the second parent is filled" do
+      it "sets the second parent as first parent" do
+      end
+    end
+
+    context "when there are siblings" do
+      it "creates siblings and add them on same child_support" do
+      end
+    end
+  end
+
+end
