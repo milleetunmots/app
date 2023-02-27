d1791f7b2e77391449a90e97c15f15615f890b96
Fabien Karquel <fabien.karquel@gmail.com>
2023-02-27 15:29:14 +0100

    chore: remove unsafe numbers in dev

--- app/services/spot_hit/send_message_service.rb
+++ app/services/spot_hit/send_message_service.rb
@@ -14,6 +14,8 @@ class SpotHit::SendMessageService
   protected
 
   def send_message(uri, form)
+    form = safeguard(form) if Rails.env.development? || ENV['SPOT_HIT_SAFEGUARD'].present?
+
     response = HTTP.post(uri, form: form)
     if JSON.parse(response.body.to_s).key? "erreurs"
       @errors << "Erreur lors de la programmation de la campagne. [Réponse SPOT_HIT API #{response.body.to_s}]"
@@ -40,5 +42,24 @@ class SpotHit::SendMessageService
       event = Event.create(event_attributes)
       @errors << "Erreur lors de la création de l'event d'envoi de message pour #{parent.phone_number}." if event.errors.any?
     end
+  end
+
+  def safeguard(form)
+    safe_numbers = ENV['SAFE_PHONE_NUMBERS'].to_s.split(',').map(&:strip)
+
+    if form['destinataires'].present?
+      numbers = form['destinataires'].split(', ').select { |num| safe_numbers.include?(num) }
+      form['destinataires'] = numbers.join(', ')
+    end
+
+    form.each_key do |key|
+      res = key.match(/destinataires\[(\+\d+)\]/)
+      next if res.nil?
+      next if safe_numbers.include?(res[1])
+
+      form.delete(key)
+    end
+
+    form
   end
 end
