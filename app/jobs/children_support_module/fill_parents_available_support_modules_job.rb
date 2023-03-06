e329e416008067bcfa181dd4e048d53546610ff0
aristidebamenou <aristide.bamenou@hotmail.com>
2023-03-06 17:56:42 +0100

    order_available_support_modules done

--- app/jobs/children_support_module/fill_parents_available_support_modules_job.rb
+++ app/jobs/children_support_module/fill_parents_available_support_modules_job.rb
@@ -31,11 +31,31 @@ class ChildrenSupportModule
 
         bilingual_child = child.child_support.is_bilingual || false
 
-        SupportModule.where("'#{child_age_range}' = ANY (age_ranges)")
-                    .where(for_bilingual: bilingual_child)
+        SupportModule.where("'#{child_age_range}' = ANY (age_ranges)").where(for_bilingual: bilingual_child)
       end
 
       def order_available_support_modules(child)
+        support_modules = find_available_support_modules(child)
+        available_support_modules_group_by_themes = {}
+        available_support_modules_ordered = []
+
+        SupportModule::THEME_LIST.each do |theme|
+          # todo enlever le pluck(:id)
+          available_support_modules_group_by_themes[theme] = support_modules.where('theme = ?', theme).pluck(:id).to_a
+        end
+
+        while !available_support_modules_group_by_themes.empty?
+          available_support_modules_group_by_themes.reject! { |theme, support_modules_array| support_modules_array.blank? }
+
+          available_support_modules_group_by_themes.each do |theme, support_modules_array|
+            available_support_modules_ordered << support_modules_array.first
+            support_modules_array.shift
+          end
+        end
+
+        available_support_modules_ordered
+
+        
       end
 
 
