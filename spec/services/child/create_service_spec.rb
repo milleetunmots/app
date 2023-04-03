63d6f4525d739a8c45caeb592c3adaf49d094838
aristidebamenou <aristide.bamenou@hotmail.com>
2023-04-03 17:58:04 +0200

    fix : create_service_spec

--- spec/services/child/create_service_spec.rb
+++ spec/services/child/create_service_spec.rb
@@ -1,16 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Child::CreateService do
-  let_it_be(:attributes) {  }
-  let_it_be(:siblings_attributes) {  }
-  let_it_be(:parent1_attributes) {  }
-  let_it_be(:mother_attributes) {  }
-  let_it_be(:father_attributes) {  }
-  let_it_be(:registration_origin) {  }
-  let_it_be(:child_min_birthdate) {  }
+  let(:birthdate) { Faker::Date.between(from: Child.min_birthdate.tomorrow, to: Child.max_birthdate.yesterday) }
+  let(:mother_attributes) {
+    {
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      phone_number: Faker::PhoneNumber.phone_number
+    }
+  }
+  let(:parent1_attributes) {
+    {
+      letterbox_name: Faker::Name.name,
+      address: Faker::Address.street_address,
+      postal_code: Faker::Address.postcode,
+      city_name: Faker::Address.city
+    }
+  }
+  let(:child_min_birthdate) { Child.min_birthdate }
 
   context "when params are valid" do
-    let_it_be(:create_service) {
+    let(:registration_origin) { nil }
+    let(:father_attributes) { {} }
+    let(:siblings_attributes) { {} }
+    let(:attributes) {
+      {
+        gender: "",
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        registration_source: Child::REGISTRATION_SOURCES.sample,
+        registration_source_details: Faker::Movies::StarWars.planet,
+        "birthdate(3i)" => birthdate.day.to_s,
+        "birthdate(2i)" => birthdate.month.to_s,
+        "birthdate(1i)" => birthdate.year.to_s
+      }
+    }
+    let(:create_service) {
       Child::CreateService.new(
         attributes,
         siblings_attributes,
@@ -25,50 +50,216 @@ RSpec.describe Child::CreateService do
       expect(create_service.child).to be_new_record
     end
 
-    # context "when registration_origin = 3" do
-    #   it "adds 'form-pro' tag" do
-    #   end
+    context "when registration_origin = 3" do
+      let(:registration_origin) { 3 }
+      let(:father_attributes) { {} }
+      let(:siblings_attributes) { {} }
+      let(:create_service) {
+        Child::CreateService.new(
+          attributes,
+          siblings_attributes,
+          parent1_attributes,
+          mother_attributes,
+          father_attributes,
+          registration_origin,
+          child_min_birthdate
+        ).call
+      }
+
+      it "adds 'form-pro' tag" do
+        expect(create_service.child.tag_list).to include "form-pro"
+      end
+
+      # it "sends sms at a specific time" do
+      # end
+    end
+
+    context "when registration_origin = 2" do
+      let(:registration_origin) { 2 }
+      let(:father_attributes) { {} }
+      let(:siblings_attributes) { {} }
+      let(:create_service) {
+        Child::CreateService.new(
+          attributes,
+          siblings_attributes,
+          parent1_attributes,
+          mother_attributes,
+          father_attributes,
+          registration_origin,
+          child_min_birthdate
+        ).call
+      }
+
+      it "adds 'form-2' tag" do
+        expect(create_service.child.tag_list).to include "form-2"
+      end
 
     #   it "sends sms at a specific time" do
     #   end
-    # end
+    end
 
-    # context "when registration_origin = 2" do
-    #   it "adds 'form-2' tag" do
-    #   end
+    context "when registration_origin is not 2 or 3" do
+      let(:registration_origin) { nil }
+      let(:father_attributes) { {} }
+      let(:siblings_attributes) { {} }
+      let(:create_service) {
+        Child::CreateService.new(
+          attributes,
+          siblings_attributes,
+          parent1_attributes,
+          mother_attributes,
+          father_attributes,
+          registration_origin,
+          child_min_birthdate
+        ).call
+      }
 
-    #   it "sends sms at a specific time" do
-    #   end
-    # end
-
-    # context "when registration_origin is not 2 or 3" do
-    #   it "adds 'form-2' site" do
-    #   end
+      it "adds 'site' tag" do
+        expect(create_service.child.tag_list).to include "site"
+      end
 
     #   it "does not send a sms" do
     #   end
-    # end
+    end
 
-    # context "when there are 2 parents" do
-    #   it "sets should_contact_parent2 to true" do
-    #   end
-    # end
+    context "when there are 2 parents" do
+      let(:registration_origin) { nil }
+      let(:father_attributes) {
+        {
+          first_name: Faker::Name.first_name,
+          last_name: Faker::Name.last_name,
+          phone_number: Faker::PhoneNumber.phone_number
+        }
+      }
+      let(:siblings_attributes) { {} }
+      let(:create_service) {
+        Child::CreateService.new(
+          attributes,
+          siblings_attributes,
+          parent1_attributes,
+          mother_attributes,
+          father_attributes,
+          registration_origin,
+          child_min_birthdate
+        ).call
+      }
 
-    # context "when only the second parent is filled" do
-    #   it "sets the second parent as first parent" do
-    #   end
-    # end
+      it "sets should_contact_parent2 to true" do
+        expect(create_service.child.should_contact_parent2).to be true
+      end
+    end
 
-    # context "when there are siblings" do
-    #   it "creates siblings and add them on same child_support" do
-    #   end
-    # end
+    context "when only the second parent is filled" do
+      let(:registration_origin) { nil }
+      let(:father_attributes) {
+        {
+          first_name: Faker::Name.first_name,
+          last_name: Faker::Name.last_name,
+          phone_number: Faker::PhoneNumber.phone_number
+        }
+      }
+      let(:siblings_attributes) { {} }
+      let(:create_service) {
+        Child::CreateService.new(
+          attributes,
+          siblings_attributes,
+          parent1_attributes,
+          {},
+          father_attributes,
+          registration_origin,
+          child_min_birthdate
+        ).call
+      }
+
+      it "sets the second parent as first parent" do
+        expect(create_service.child.parent1.first_name).to eq father_attributes[:first_name]
+        expect(create_service.child.parent1.last_name).to eq father_attributes[:last_name]
+        expect(create_service.child.parent2).to be nil
+      end
+    end
+
+    context "when there are siblings" do
+      let(:first_sibling_birthdate) { Faker::Date.between(from: Child.min_birthdate.tomorrow, to: Child.max_birthdate.yesterday) }
+      let(:second_sibling_birthdate) { Faker::Date.between(from: Child.min_birthdate.tomorrow, to: Child.max_birthdate.yesterday) }
+
+      let(:registration_origin) { nil }
+      let(:father_attributes) {
+        {
+          first_name: Faker::Name.first_name,
+          last_name: Faker::Name.last_name,
+          phone_number: Faker::PhoneNumber.phone_number
+        }
+      }
+      let(:siblings_attributes) {
+        [
+          {
+            gender: "",
+            first_name: Faker::Name.first_name,
+            last_name: Faker::Name.last_name,
+            "birthdate(3i)" => first_sibling_birthdate.day.to_s,
+            "birthdate(2i)" => first_sibling_birthdate.month.to_s,
+            "birthdate(1i)" => first_sibling_birthdate.year.to_s
+          },
+          {
+            gender: "",
+            first_name: Faker::Name.first_name,
+            last_name: Faker::Name.last_name,
+            "birthdate(3i)" => second_sibling_birthdate.day.to_s,
+            "birthdate(2i)" => second_sibling_birthdate.month.to_s,
+            "birthdate(1i)" => second_sibling_birthdate.year.to_s
+          }
+        ]
+      }
+      let(:create_service) {
+        Child::CreateService.new(
+          attributes,
+          siblings_attributes,
+          parent1_attributes,
+          mother_attributes,
+          father_attributes,
+          registration_origin,
+          child_min_birthdate
+        ).call
+      }
+
+      it "creates siblings and add them on same child_support" do
+        expect(create_service.child.child_support.children.count).to eq 3
+      end
+    end
   end
 
-  # context "when params are not valid" do
-  #   context "when child attributes are not valid" do
-  #     it "does not create child" do
-  #     end
+  context "when params are not valid" do
+    let(:registration_origin) { nil }
+    let(:father_attributes) { {} }
+    let(:siblings_attributes) { {} }
+    let(:attributes) {
+      {
+        gender: "",
+        first_name: nil,
+        last_name: nil,
+        registration_source: Child::REGISTRATION_SOURCES.sample,
+        registration_source_details: Faker::Movies::StarWars.planet,
+        "birthdate(3i)" => birthdate.day.to_s,
+        "birthdate(2i)" => birthdate.month.to_s,
+        "birthdate(1i)" => birthdate.year.to_s
+      }
+     }
+     let(:create_service) {
+      Child::CreateService.new(
+        attributes,
+        siblings_attributes,
+        parent1_attributes,
+        mother_attributes,
+        father_attributes,
+        registration_origin,
+        child_min_birthdate
+      ).call
+    }
+
+    context "when child attributes are not valid" do
+      it "does not create child" do
+        expect(create_service.child).not_to be_valid
+      end
 
       # context "when registration_origin = 2" do
       #   it "does not send sms" do
@@ -119,6 +310,6 @@ RSpec.describe Child::CreateService do
     #     it "does not send sms" do
     #     end
     #   end
-    # end
-  # end
+    end
+  end
 end
