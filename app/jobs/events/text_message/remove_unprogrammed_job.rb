151a77caa6e188ad74e23da7759d45e77a7f2a92
aristidebamenou <aristide.bamenou@hotmail.com>
2023-04-14 11:09:38 +0200

    feature: suppression des messages non programmés

--- app/jobs/events/text_message/remove_unprogrammed_job.rb
+++ app/jobs/events/text_message/remove_unprogrammed_job.rb
@@ -4,13 +4,15 @@ module Events
     class RemoveUnprogrammedJob < ApplicationJob
 
       def perform
-        Events::TextMessage.sent_by_app_text_messages.where(spot_hit_status: 0).where('occurred_at < ?', Time.zone.now).find_each do |text_message|
+        Events::TextMessage.sent_by_app_text_messages.where(spot_hit_status: 0).find_each do |text_message|
           params = {
             key: ENV['SPOT_HIT_API_KEY'],
             id: text_message.spot_hit_message_id,
-
+            date_start: text_message.occured_at.prev_day.to_i,
+            date_end: text_message.occured_at.next_day.to_i
           }
-
+          check = spothit_check(params.merge(product: 'sms')) || spothit_check(params.merge(product: 'mms'))
+          text_message.destroy if check.nil?
         end
       end
 
@@ -20,10 +22,12 @@ module Events
         if response.key? 'erreurs'
           raise "Error: #{response.body}]"
         elsif response.size.zero?
+          nil
           # response looks like
           # [ [ "245174", "test2", "", "1", "1681370520", "0", "", "sms", "1681284185", "+33637572453" ] ]
           # so an empty array means the message does not exist in spothit
-
+        else
+          response.first
         end
       end
     end
