ec957ebcecaf6d7b7f86118d0a73853eb24a605e
aristidebamenou <aristide.bamenou@hotmail.com>
2023-04-19 17:00:14 +0200

    feature : par défaut, on attribue au second parent le choix du premier parent s'il existe

--- app/jobs/children_support_module/select_default_support_module_job.rb
+++ app/jobs/children_support_module/select_default_support_module_job.rb
@@ -6,8 +6,13 @@ class ChildrenSupportModule
       group = Group.find(group_id)
       group.children.where(group_status: 'active').find_each do |child|
         child.children_support_modules.where(support_module: nil).each do |csm|
-          first_support_module_id = csm.available_support_module_list.reject(&:blank?).first
-          csm.update(support_module_id: first_support_module_id)
+          the_other_parent = csm.parent == csm.child.parent1 ? csm.child.parent2 : csm.child.parent1
+          if the_other_parent.children_support_modules.where.not(support_module: nil).count > csm.parent.children_support_modules.where.not(support_module: nil).count
+            csm.update(support_module_id: the_other_parent.children_support_modules.last.support_module.id)
+          else
+            first_support_module_id = csm.available_support_module_list.reject(&:blank?).first
+            csm.update(support_module_id: first_support_module_id)
+          end
         end
       end
     end
