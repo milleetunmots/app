c2dd82c527c8a51fa516911ed6dbee9c5cf9dd31
aristidebamenou <52001075+aristidebamenou@users.noreply.github.com>
2023-04-21 16:04:21 +0200

    Feature/parents choice updated (#518)
    * feature : Quand le parent 1 décroche à l'appel 2, son choix de module est attribué au parent 2 aussi.
    
    * feature : par défaut, on attribue au second parent le choix du premier parent s'il existe
    
    * feature : on garde les ChildrenSupportModules pour les 2 parents mais on n'envoi le suivi via SMS que pour l'enfant le plus jeune (current_child)
    
    * refacto: simplify conditions
    
    ---------
    
    Co-authored-by: Fabien Karquel <fabien@karquel.com>

--- app/jobs/children_support_module/program_support_module_sms_job.rb
+++ app/jobs/children_support_module/program_support_module_sms_job.rb
@@ -5,7 +5,16 @@ class ChildrenSupportModule
     def perform(group_id, first_message_date)
       errors = {}
       group = Group.find(group_id)
-      children_support_module_ids = ChildrenSupportModule.where(child_id: group.children.where(group_status: "active").ids)
+
+      current_children = group.children.where(group_status: 'active').map do |child|
+        child.siblings.where(group: group, group_status: 'active').order(:birthdate).last
+      end
+
+      not_current_children = group.children.where(group_status: 'active').reject do |child|
+        child == child.siblings.where(group: group, group_status: 'active').order(:birthdate).last
+      end
+
+      children_support_module_ids = ChildrenSupportModule.where(child_id: current_children).ids
 
       check_service = ChildrenSupportModule::CheckCreditsService.new(children_support_module_ids).call
       raise check_service.errors.to_json if check_service.errors.any?
@@ -14,6 +23,8 @@ class ChildrenSupportModule
       errors[group.id] = service.errors if service.errors.any?
 
       raise errors.to_json if errors.any?
+
+      ChildrenSupportModule.where(child_id: not_current_children).update_all(is_programmed: true)
     end
   end
 end
