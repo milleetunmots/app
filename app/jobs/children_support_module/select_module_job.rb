1fe3c9882c4c96b50230251222462b738290e0a8
Fabien Karquel <fabien@karquel.com>
2023-05-10 10:21:45 +0200

    feat: Send only one sms to a family to choose their module when they have multiple children in the same group

--- app/jobs/children_support_module/select_module_job.rb
+++ app/jobs/children_support_module/select_module_job.rb
@@ -4,11 +4,13 @@ class ChildrenSupportModule
       errors = {}
       group = Group.find(group_id)
 
-      group.children.where(group_status: "active").find_each do |child|
+      group.children.where(group_status: 'active').find_each do |child|
+        next if child.siblings_on_same_group.count > 1 && child.child_support.current_child != child
+
         service = ChildSupport::SelectModuleService.new(
           child,
           select_module_date.sunday? ? select_module_date.next_day : select_module_date,
-          "12:30"
+          '12:30'
         ).call
         errors[child.id] = service.errors if service.errors.any?
       end
