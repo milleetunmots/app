5bb551df68f5c73287ebe360ace5da23eda09a2b
Fabien Karquel <fabien@karquel.com>
2023-05-10 10:43:14 +0200

    complete tests and add factory

--- spec/services/child_support/select_module_service_spec.rb
+++ spec/services/child_support/select_module_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ChildSupport::SelectModuleService do
   let(:child) { FactoryBot.create(:child, should_contact_parent1: true) }
   let(:support_module_list) { FactoryBot.create_list(:support_module, 3) }
-  let(:child_support) do
+  let!(:child_support) do
     FactoryBot.create(
       :child_support,
       current_child: child,
@@ -33,24 +33,36 @@ RSpec.describe ChildSupport::SelectModuleService do
 
   context "when there aren't children support module not programmed for the child and their parent" do
     before { expect_any_instance_of(ProgramMessageService).to receive(:call).and_return(ProgramMessageService.new("01/01/2020", "12:30", [], '')) }
-    it "it's created" do
-      child_support
+    it 'creates ChldrenSupportModule' do
       expect { subject }.to change { ChildrenSupportModule.count }.by(1)
     end
   end
 
   context 'when there are only one available support module for the child and their parent' do
-    it "it's choosen" do
+    it 'choose the support_module directly and does not send message' do
       child_support.update!(parent1_available_support_module_list: [support_module_list.first.id])
       expect { subject }.to change { ChildrenSupportModule.count }.by(1)
       expect(ChildrenSupportModule.find_by(child_id: child.id, parent_id: child.parent1_id).support_module_id).to eq support_module_list.first.id
+      expect_any_instance_of(ProgramMessageService).not_to receive(:call)
+    end
+  end
+
+  context "when there is already a ChildrenSupportModule" do
+    before do
+      FactoryBot.create(:children_support_module, child: child, parent: child.parent1, support_module: nil)
+      expect_any_instance_of(ProgramMessageService).to receive(:call).and_return(ProgramMessageService.new("01/01/2020", "12:30", [], ''))
+    end
+    it 'does not create a new ChldrenSupportModule' do
+      expect { subject }.to change { ChildrenSupportModule.count }.by(0)
     end
   end
 
   context 'when selection messages are sent to parents' do
-    it 'CheckToSendReminderJob is programmed' do
-      # subject
-      # expect_any_instance_of(SpotHit::SendSmsService).not_to receive(:call)
+    it 'ChildrenSupportModule::CheckToSendReminderJob is programmed' do
+      expect_any_instance_of(ProgramMessageService).to receive(:call).and_return(ProgramMessageService.new("01/01/2020", "12:30", [], ''))
+      ActiveJob::Base.queue_adapter = :test
+
+      expect { subject }.to have_enqueued_job(ChildrenSupportModule::CheckToSendReminderJob).exactly(:once)
     end
   end
 end
