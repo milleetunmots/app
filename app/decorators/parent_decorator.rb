0e2d9837b541dee80a4227c195516a37d7f5e6de
aristidebamenou <52001075+aristidebamenou@users.noreply.github.com>
2023-05-24 10:47:25 +0200

    feature : affichage des choix de modules (#536)
    * feature : affichage des choix de modules
    
    * group support_modules by child
    
    ---------
    
    Co-authored-by: Fabien Karquel <fabien@karquel.com>

--- app/decorators/parent_decorator.rb
+++ app/decorators/parent_decorator.rb
@@ -122,11 +122,19 @@ class ParentDecorator < BaseDecorator
 
   def selected_support_module
     arbre do
-      model.children_support_modules.includes(:support_module).decorate.each do |children_support_module|
-        div do
-          a "#{children_support_module.name} - #{children_support_module.created_at.strftime("%d/%m/%Y")}", href: admin_children_support_module_path(children_support_module),
-            class: 'available_support_module', target: '_blank'
-          text_node "&nbsp;".html_safe
+      model.children_support_modules.includes(:support_module).decorate.group_by(&:child_id).each do |_, children_support_modules|
+        div(style: 'margin-bottom: 10px;') do
+          children_support_modules.each do |children_support_module|
+            display_selected_module = "#{children_support_module.name} - #{children_support_module.created_at.strftime("%d/%m/%Y")}"
+            display_selected_module = "#{display_selected_module} - #{children_support_module.child.first_name}" if children_support_module.child.have_siblings_on_same_group?
+            div do
+              a display_selected_module,
+                href: admin_children_support_module_path(children_support_module),
+                class: 'available_support_module',
+                target: '_blank'
+              text_node "&nbsp;".html_safe
+            end
+          end
         end
       end
     end
