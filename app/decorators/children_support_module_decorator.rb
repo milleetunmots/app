4ec890744aef643be2dd0a94a924d2b863f5cc82
aristidebamenou <aristide.bamenou@hotmail.com>
2023-05-25 15:55:41 +0200

    feature : modules choisis dans csv

--- app/decorators/children_support_module_decorator.rb
+++ app/decorators/children_support_module_decorator.rb
@@ -2,17 +2,33 @@ class ChildrenSupportModuleDecorator < BaseDecorator
 
   def name_display
     return support_module.decorate.admin_link(label: support_module.decorate.name_with_tags) if support_module
-    return "Laisse le choix à 1001mots" if is_completed
+    return 'Laisse le choix à 1001mots' if is_completed
 
     "Pas encore choisi"
+  end
+
+  def name_display_for_csv
+    return support_module.decorate.name_with_tags if support_module
+
+    return 'Laisse le choix à 1001mots' if is_completed
+
+    'Pas encore choisi'
   end
 
   def parent_name
     parent.decorate.admin_link
   end
 
+  def parent_name_for_csv
+    parent.decorate.name
+  end
+
   def child_name
     child.decorate.admin_link
+  end
+
+  def child_name_for_csv
+    child.decorate.name
   end
 
   def name_with_date
@@ -24,5 +40,9 @@ class ChildrenSupportModuleDecorator < BaseDecorator
 
   def available_support_module_names
     model.available_support_modules.decorate.map { |support_module| support_module.admin_link(label: support_module.name_with_tags) }
+  end
+
+  def available_support_module_names_for_csv
+    model.available_support_modules.decorate.map(&:name_with_tags)
   end
 end
