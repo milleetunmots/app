6e4430086ecb40c19e608465196a4642bc1af412
aristidebamenou <aristide.bamenou@hotmail.com>
2023-05-26 17:17:28 +0200

    fix : update before migration

--- db/migrate/20230524091902_not_nil_for_bilingual.rb
+++ db/migrate/20230524091902_not_nil_for_bilingual.rb
@@ -1,6 +1,6 @@
 class NotNilForBilingual < ActiveRecord::Migration[6.0]
   def change
-    SupportModule.where(for_bilingual: nil).update_all(for_bilingual: false)
+    SupportModule.with_discarded.where(for_bilingual: nil).update_all(for_bilingual: false)
 
     change_column :support_modules, :for_bilingual, :boolean, null: false, default: false
   end
