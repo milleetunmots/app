d2711344f9022eb3138ef46bcde0576ac47f9d8e
Fabien Karquel <fabien.karquel@gmail.com>
2023-06-20 17:44:45 +0200

    more tests

--- spec/jobs/create_children_support_module_job_spec.rb
+++ spec/jobs/create_children_support_module_job_spec.rb
@@ -55,6 +55,24 @@ RSpec.describe ChildrenSupportModule::CreateChildrenSupportModuleJob, type: :job
           expect(csm.available_support_module_list).to eq(child1.child_support.parent1_available_support_module_list)
         end
       end
+
+      context 'with one child_support_module not programmed' do
+        let!(:csm_1) { FactoryBot.create(:children_support_module, child: child1, parent: child1.parent1, is_programmed: false) }
+        let!(:csm_2) { FactoryBot.create(:children_support_module, child: child2, parent: child2.parent1, is_programmed: true) }
+
+        it 'creates one children_support_module only for child that has no children_support_module already programmed' do
+          expect {
+            subject.perform_now(group.id) rescue nil
+          }.to change { ChildrenSupportModule.count }.by(1)
+
+          expect(ChildrenSupportModule.where(child_id: child1.id, parent_id: child1.parent1.id).count).to eq(1)
+          expect(ChildrenSupportModule.where(child_id: child2.id, parent_id: child2.parent1.id).count).to eq(2)
+          expect(ChildrenSupportModule.where(child_id: child1.id, parent_id: child1.parent1.id, is_programmed: false).count).to eq(1)
+          expect(ChildrenSupportModule.where(child_id: child2.id, parent_id: child2.parent1.id, is_programmed: false).count).to eq(1)
+        end
+
+        it { expect { subject.perform_now(group_id) }.to raise_error }
+      end
     end
   end
 end
