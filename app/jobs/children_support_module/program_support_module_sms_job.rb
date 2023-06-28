629479126c657cb0c72833c03f8c753609f54140
Fabien Karquel <fabien@karquel.com>
2023-06-28 09:35:54 +0200

    fix(ProgramSupportModuleSms): create a task to logistic members when not enough credits

--- app/jobs/children_support_module/program_support_module_sms_job.rb
+++ app/jobs/children_support_module/program_support_module_sms_job.rb
@@ -17,7 +17,18 @@ class ChildrenSupportModule
       children_support_module_ids = ChildrenSupportModule.where(child_id: current_children).ids
 
       check_service = ChildrenSupportModule::CheckCreditsService.new(children_support_module_ids).call
-      raise check_service.errors.to_json if check_service.errors.any?
+      if check_service.errors.any?
+        logistics_team_members = AdminUser.all_logistics_team_members
+        logistics_team_members.each do |ltm|
+          Task.create(
+            assignee_id: ltm.id,
+            title: "La programmation des sms de la cohorte \"#{group.name}\" a été annulé car il n'y a pas assez de crédits",
+            description: check_service.errors.join("<br>"),
+            due_date: Date.today
+          )
+        end
+        raise check_service.errors.to_json
+      end
 
       service = ChildSupport::ProgramChosenModulesService.new(children_support_module_ids, first_message_date).call
       errors[group.id] = service.errors if service.errors.any?
