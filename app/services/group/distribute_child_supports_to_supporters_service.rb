7744fbb4ef9e6c1c99eb718587cc534a76031b91
aristidebamenou <aristide.bamenou@hotmail.com>
2023-07-07 17:37:41 +0200

    feature : balance fixed

--- app/services/group/distribute_child_supports_to_supporters_service.rb
+++ app/services/group/distribute_child_supports_to_supporters_service.rb
@@ -28,11 +28,7 @@ class Group
       total_capacity = @child_supports_count_by_supporter.sum { |h| h[:child_supports_count] }
       total_child_supports_count = @group.child_supports.joins(:children).where(children: { group_status: 'active' }).uniq.count
 
-      # if total_capacity < total_child_supports_count
-        # @child_supports_count_by_supporter.sort_by! { |child_support_count_by_supporter| child_support_count_by_supporter[:child_supports_count] }
-      # else
-        @child_supports_count_by_supporter.sort! { |first, second| second[:child_supports_count] <=> first[:child_supports_count] }
-      # end
+      @child_supports_count_by_supporter.sort_by! { |child_support_count_by_supporter| child_support_count_by_supporter[:child_supports_count] }
 
       while total_capacity != total_child_supports_count
         @child_supports_count_by_supporter.each do |supporter_capacity|
@@ -44,6 +40,8 @@ class Group
           total_capacity += sign
         end
       end
+
+      @child_supports_count_by_supporter.sort! { |first, second| second[:child_supports_count] <=> first[:child_supports_count] }
     end
 
     def order_child_supports
