6ae5663454d9263deeeb4081c7b626ec4549d77f
aristidebamenou <aristide.bamenou@hotmail.com>
2023-10-19 15:57:26 +0200

    feature : Création de tâches en cas de crédit insuffisant

--- app/services/group/program_sms_to_bilinguals_service.rb
+++ app/services/group/program_sms_to_bilinguals_service.rb
@@ -8,14 +8,17 @@ class Group
     SECOND_MESSAGE_LINK_ID = RedirectionTarget.joins(:medium).find_by(media: { name: 'Bilinguisme - Pour debuter - 12-17' }).id
 
     def initialize(group_id, first_message_date)
-      @children = Group.find(group_id).bilingual_children.pluck(:id).map { |id| "child.#{id}" }
+      @group = Group.find(group_id)
+      @children = @group.bilingual_children.pluck(:id).map { |id| "child.#{id}" }
       @first_message_date = first_message_date
       @sms_to_send_count = 0
+      @errors = []
     end
 
     def call
       sms_count
-      raise "Impossible de programmer les messages aux familles bilingues car il n'y a pas assez de crédit spot-hit" unless check_credits
+      check_credits
+      raise "Impossible de programmer les messages aux familles bilingues car il n'y a pas assez de crédit spot-hit" if @errors.any?
 
       program_message(date: @first_message_date, message: FIRST_MESSAGE)
       program_message(date: @first_message_date + 7.days, message: SECOND_MESSAGE, link_id: SECOND_MESSAGE_LINK_ID)
@@ -29,17 +32,24 @@ class Group
     def sms_count
       second_message = SECOND_MESSAGE.gsub('{URL}', 'https://app.1001mots.org/r/xxxxxx/xx')
       @sms_to_send_count += ((FIRST_MESSAGE + second_message + THIRD_MESSAGE).size / 160) + 1
-      @sms_to_send_count * @children.count
+      @sms_to_send_count *= @children.count
     end
 
     def check_credits
       credit_service = SpotHit::GetCreditsService.new.call
-      errors = credit_service.errors
-      raise errors.join('\n') if errors.any?
+      raise credit_service.errors.join('\n') if credit_service.errors.any?
 
-      raise "Pas assez de crédits sms sur SPOT-HIT : #{@sms_to_send_count} crédits sont nécéssaires." if @sms_to_send_count > credit_service.sms
+      return if @sms_to_send_count <= credit_service.sms
 
-      true
+      @errors << "Pas assez de crédits sms sur SPOT-HIT : #{@sms_to_send_count} crédits sont nécéssaires."
+      AdminUser.all_logistics_team_members.each do |ltm|
+        Task.create(
+          assignee_id: ltm.id,
+          title: "Il n'y a pas assez de crédits pour la programmation des messages aux familles bilingues de la cohorte : \"#{@group.name}\"",
+          description: @errors.join('\n'),
+          due_date: Time.zone.today
+        )
+      end
     end
 
     def program_message(date:, message:, link_id: nil, hour: '12:31', children: @children)
