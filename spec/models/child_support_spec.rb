2930a2244cda27c74c276026e16a265e64cf7dad
aristidebamenou <52001075+aristidebamenou@users.noreply.github.com>
2023-12-01 15:43:55 +0100

    Feat/save support module choices in child support bis (#686)
    * feat(child supports) : register modules chosen by parents
    
    * fix(children support modules) : add conditions to save_chosen_module_to_child_support callback
    
    * feat(children_support_modules) : save_chosen_module_to_child_support uses module_index and handle modules > 5
    
    * fix : avoid error if module_index is nil in save_chosen_module_to_child_support
    
    ---------
    
    Co-authored-by: Maxime Martin <dev@maxmartin.fr>

--- spec/models/child_support_spec.rb
+++ spec/models/child_support_spec.rb
@@ -122,6 +122,10 @@
 #  will_stay_in_group                    :boolean          default(FALSE), not null
 #  created_at                            :datetime         not null
 #  updated_at                            :datetime         not null
+#  module2_chosen_by_parents_id          :bigint
+#  module3_chosen_by_parents_id          :bigint
+#  module4_chosen_by_parents_id          :bigint
+#  module5_chosen_by_parents_id          :bigint
 #  supporter_id                          :bigint
 #
 # Indexes
@@ -142,6 +146,10 @@
 #  index_child_supports_on_call5_language_awareness               (call5_language_awareness)
 #  index_child_supports_on_call5_parent_progress                  (call5_parent_progress)
 #  index_child_supports_on_discarded_at                           (discarded_at)
+#  index_child_supports_on_module2_chosen_by_parents_id           (module2_chosen_by_parents_id)
+#  index_child_supports_on_module3_chosen_by_parents_id           (module3_chosen_by_parents_id)
+#  index_child_supports_on_module4_chosen_by_parents_id           (module4_chosen_by_parents_id)
+#  index_child_supports_on_module5_chosen_by_parents_id           (module5_chosen_by_parents_id)
 #  index_child_supports_on_parent1_available_support_module_list  (parent1_available_support_module_list) USING gin
 #  index_child_supports_on_parent2_available_support_module_list  (parent2_available_support_module_list) USING gin
 #  index_child_supports_on_should_be_read                         (should_be_read)
@@ -149,6 +157,10 @@
 #
 # Foreign Keys
 #
+#  fk_rails_...  (module2_chosen_by_parents_id => support_modules.id)
+#  fk_rails_...  (module3_chosen_by_parents_id => support_modules.id)
+#  fk_rails_...  (module4_chosen_by_parents_id => support_modules.id)
+#  fk_rails_...  (module5_chosen_by_parents_id => support_modules.id)
 #  fk_rails_...  (supporter_id => admin_users.id)
 #
 
