679737c9934a071f7307b29627fa40d0a79a3e92
aristidebamenou <52001075+aristidebamenou@users.noreply.github.com>
2024-01-17 11:52:00 +0100

    feature : ajout des tranches d'ages (#710)
    * feature : ajout des tranches d'ages
    
    * feat(group) : only return children_support_modules of children with group_status active in children_support_modules_informations_service
    
    * fix(group) : children_support_modules_informations_service file and zip name
    
    * feature : ages dans une nouvelle colonne
    
    ---------
    
    Co-authored-by: Maxime Martin <dev@maxmartin.fr>

--- app/services/group/children_support_modules_informations_service.rb
+++ app/services/group/children_support_modules_informations_service.rb
@@ -4,21 +4,27 @@ class Group
     require 'fast_excel'
     require 'zip'
 
-    attr_reader :zip_file
+    attr_reader :zip_file, :zip_filename
 
-    COLUMNS = %w[Module Effectif].freeze
+    COLUMNS = %w[Module Âges Effectif].freeze
 
     def initialize(group_id, index)
       @group = Group.find(group_id)
       @index = index
-      @support_modules_count = Hash.new(0)
+      @support_modules_count = {}
       @workbook = FastExcel.open
+      group_without_module_zero = @group.started_at < DateTime.parse(ENV['MODULE_ZERO_FEATURE_START'])
+      @module_num = group_without_module_zero ? @index : @index.to_i - 1
+      @zip_filename = "Cohorte #{@group.name} - Choix modules #{@module_num}.zip"
     end
 
     def call
       child_and_parent1_ids.each do |child_id, parent1_id|
         csm = ChildrenSupportModule.with_support_module.find_by(child_id: child_id, parent_id: parent1_id, module_index: @index)
-        @support_modules_count[csm.support_module.name.to_sym] += 1 if csm
+        next unless csm
+
+        @support_modules_count[csm.support_module.name] ||= Hash.new(0)
+        @support_modules_count[csm.support_module.name][csm.support_module.decorate.display_age_ranges.to_sym] += 1
       end
 
       init_excel_file
@@ -31,18 +37,17 @@ class Group
     private
 
     def child_and_parent1_ids
-      @group.children.pluck(:id, :parent1_id)
+      @group.children.active_group.pluck(:id, :parent1_id)
     end
 
     def init_excel_file
-
       @worksheet = @workbook.add_worksheet
       @worksheet.append_row(COLUMNS, @workbook.add_format(bold: true, bg_color: :'#70AD47', font_color: :white))
     end
 
     def fill_exel_file
-      @support_modules_count.each do |support_module, count|
-        @worksheet.append_row([support_module, count])
+      @support_modules_count.each do |support_module, ages_count|
+        @worksheet.append_row([support_module, ages_count.keys.first, ages_count.values.first])
       end
       @worksheet.set_columns_width(0, 1, width = 25)
       # @worksheet.set_columns_width(1, 2, width = 20)
@@ -59,7 +64,7 @@ class Group
         temp.write(@workbook.read_string)
         temp.rewind
 
-        zipfile.add "choix-modules-#{@index.to_i.positive? ? @index.to_i - 1 : 'pas-programmé'}.xlsx", temp.path
+        zipfile.add "choix-modules-#{@index.to_i.positive? ? @module_num : 'pas-programmé'}.xlsx", temp.path
       end
 
       # Store tempfiles in an array so they are not automatically removed by the garbage collector
