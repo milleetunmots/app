f8745ee56ccedb2dc8d62669dcdb7eb80e731b09
aristidebamenou <aristide.bamenou@hotmail.com>
2024-02-28 08:39:40 +0100

    feature : stop unassigned_number

---
+++ app/services/child/stop_unassigned_number_service.rb
@@ -0,0 +1,19 @@
+class Child::StopUnassignedNumberService < ProgramMessageService
+
+  def initialize
+    @child_supports = ChildSupport.with_a_child_in_active_group.with_unassigned_number
+  end
+
+  def call
+    @child_supports.find_each do |child_support|
+      call_status = []
+      (0..5).each do |call_idx|
+        call_status << child_support.send("call#{call_idx}_status")
+      end
+      call_status.reject!(&:blank?)
+      next if call_status.last != 'Numéro erroné'
+
+      child_support.children.update(group_status: 'stopped')
+    end
+  end
+end
