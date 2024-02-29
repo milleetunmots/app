e457c2b5698cc5f94b0baad4d2bfb6e7efbd9b41
aristidebamenou <aristide.bamenou@hotmail.com>
2024-02-29 17:02:51 +0100

    fix(import_from_airtable_service) : import only verified urls

--- app/models/airtables/url.rb
+++ app/models/airtables/url.rb
@@ -3,4 +3,7 @@ class Airtables::Url < Airrecord::Table
   self.base_key = 'app9OVHqyideRP6od'.freeze
   self.table_name = 'tblCCRGXNXRL0WU5P'.freeze
 
+  def self.verified
+    all(filter: '{Status} = "Lien vérifié"')
+  end
 end
--- app/services/video/import_from_airtable_service.rb
+++ app/services/video/import_from_airtable_service.rb
@@ -3,7 +3,7 @@ class Video::ImportFromAirtableService
   attr_reader :new_videos, :updated_videos
 
   def initialize
-    @airtable_videos = Airtables::Url.all.map { |video| { id: video.id, name: video['Nom URL base Yann'], url: video['Lien URL cible'] } }
+    @airtable_videos = Airtables::Url.verified.map { |video| { id: video.id, name: video['Nom URL base Yann'], url: video['Lien URL cible'] } }
     @new_videos = []
     @updated_videos = []
   end
@@ -16,9 +16,8 @@ class Video::ImportFromAirtableService
       if video.nil?
         new_video = Media::Video.create!(airtable_id: airtable_video[:id], name: airtable_video[:name], url: airtable_video[:url])
         @new_videos << new_video.id
-      elsif video.name != airtable_video[:name]
-        video.update!(name: airtable_video[:name])
-        @updated_videos << video.id
+      else
+        @updated_videos << video.id if video.update!(name: airtable_video[:name], url: airtable_video[url])
       end
     end
     self
