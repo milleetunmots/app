2f0493dadae7859d3075fe51c42b31abcd19132b
aristidebamenou <aristide.bamenou@hotmail.com>
2024-03-01 17:17:02 +0100

    feature(add_to_group_service) : Add siblings

--- app/services/child/add_to_group_service.rb
+++ app/services/child/add_to_group_service.rb
@@ -5,52 +5,40 @@ class Child
     attr_reader :child, :group
 
     def initialize(child_id)
-      @child = Child.find(child_id)
       @group = nil
+      @siblings = Child.find(child_id).siblings
+      # Le oldest de moins de 30 mois
+      @oldest_child = @siblings.order(:birthdate).first
     end
 
     def call
-      # Quand l'enfant est seul
-      if @child.siblings.count == 1
-        retrieve_next_available_group(@child)
-        @child.update(group: @group, group_status: 'active') if @group
-      else
-        # Aucun membre de la fratrie n'est suivi
-        assign_group_to_all_siblings and return self if @child.siblings.none? { |sibling| sibling.group_status == 'active' }
-
-        return self if @child.siblings.none? { |sibling| sibling.group_id }
-
-        # At the start of each module >= 2 (FillParentsAvailableSupportModulesJob), we add siblings >= 6 months to the group
-        return self if @child.siblings.any? { |sibling| sibling.group&.started? && sibling.group_status == 'active' }
-
-        # Au moins un membre de la fratrie est suivi mais le groupe n'a pas commencé
-        join_sibling_group
-      end
+      retrieve_next_available_group
+      add_to_group
       self
     end
 
     private
 
-    def retrieve_next_available_group(child)
-      @group = child.months >= 4 ? Group.next_available_at(Time.zone.today) : Group.next_available_at(child.birthdate + 4.months)
+    def retrieve_next_available_group
+      @group = @siblings.first_active_group || (@oldest_child.months >= 4 ? Group.next_available_at(Time.zone.today) : Group.next_available_at(@oldest_child.birthdate + 4.months))
     end
 
-    def assign_group_to_all_siblings
-      retrieve_next_available_group(@child.youngest_sibling)
-      return self unless @group
+    def add_to_group
+      return unless @group
 
-      @child.siblings.each do |sibling|
-        next if sibling.birthdate + 30.months < @group.started_at
+      if @group.started?
+        @siblings.each do |child|
+          next if child.birthdate + 6.months > @group.started_at
 
-        sibling.update(group: @group, group_status: 'active')
+          child.update(group: @group, group_status: child.months >= 36 ? 'stopped' : 'active')
+        end
+      else
+        @siblings.each do |child|
+          next if child.birthdate + 4.months > @group.started_at
+
+          child.update(group: @group, group_status: child.months >= 30 ? 'stopped' : 'active')
+        end
       end
-    end
-
-    def join_sibling_group
-      sibling_group = @child.siblings.where(group_status: 'active').first.group
-      return if @child.birthdate + 4.months < sibling_group.started_at
-
-      @child.update(group: sibling_group, group_status: 'active')
     end
   end
 end
