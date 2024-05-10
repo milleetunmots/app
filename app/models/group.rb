e2833a46a0b30d5a52daa627574536f29a7c85f8
aristidebamenou <aristide.bamenou@hotmail.com>
2024-05-10 12:21:13 +0200

    feature(group) : Callback to standardize the name of creating group

--- app/models/group.rb
+++ app/models/group.rb
@@ -54,11 +54,13 @@ class Group < ApplicationRecord
   scope :ended, -> { where('ended_at <= ?', Time.zone.today) }
   scope :not_started, -> { where('started_at >= ? AND support_module_programmed = ?', Time.zone.today, 0) }
   scope :started, -> { where('started_at < ? OR support_module_programmed > ?', Time.zone.today, 0) }
+  scope :effective_group, -> { where.not(started_at: nil).where('expected_children_number > ?', 0) }
 
   # ---------------------------------------------------------------------------
   # callbacks
   # ---------------------------------------------------------------------------
 
+  before_create :standardize_name
   after_create :add_waiting_children
 
   # ---------------------------------------------------------------------------
@@ -111,6 +113,14 @@ class Group < ApplicationRecord
   def add_waiting_children
     # Add waiting children to next available group for them (could be another group)
     Child::AddWaitingChildrenToGroupJob.perform_later
+  end
+
+  def standardize_name
+    return if expected_children_number.zero?
+
+    last_group = Group.effective_group.where('extract(month from started_at) = ? AND extract(year from started_at) = ?', started_at.month, started_at.year).order(:started_at).last
+    index = last_group.present? ? last_group.name.split('-').last.to_i + 1 : 1
+    self.name = "#{started_at.strftime('%Y/%m/%d')} - #{I18n.t started_at.strftime('%B')}#{started_at.strftime('%y')} - #{index}"
   end
 
   # ---------------------------------------------------------------------------
