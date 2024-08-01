7071e511311e4817e64d66e5d622c7af1e4575d1
Maxime Martin <dev@maxmartin.fr>
2024-08-01 12:50:32 +0200

    fix(children_support_modules) : CheckToSendReminderJob error handling

--- app/jobs/children_support_module/check_to_send_reminder_job.rb
+++ app/jobs/children_support_module/check_to_send_reminder_job.rb
@@ -1,6 +1,7 @@
 class ChildrenSupportModule
   class CheckToSendReminderJob < ApplicationJob
     def perform(children_support_module_ids, reminder_date, second_reminder = false)
+      @errors = {}
       if children_support_module_ids.instance_of?(Array)
         children_support_module_ids.each do |children_support_module_id|
           send_reminder(children_support_module_id, reminder_date, second_reminder)
@@ -8,6 +9,7 @@ class ChildrenSupportModule
       else
         send_reminder(children_support_module_ids, reminder_date, second_reminder)
       end
+      raise @errors.to_json if @errors.any?
     end
 
     private
@@ -16,7 +18,7 @@ class ChildrenSupportModule
       children_support_module = ChildrenSupportModule.find(children_support_module_id)
       unless children_support_module.support_module.present? || children_support_module.is_completed
         service = ChildrenSupportModule::SendReminder.new(children_support_module, reminder_date, second_reminder).call
-        raise service.errors if service.errors.any?
+        @errors[children_support_module_id] = service.errors if service.errors.any?
       end
     end
   end
