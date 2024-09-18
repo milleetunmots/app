480872a32658571636d23de2bf450081d89a06bc
aristidebamenou <aristide.bamenou@hotmail.com>
2024-09-18 16:39:13 +0200

    feature(history) : display message status

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -40,6 +40,23 @@ class EventDecorator < BaseDecorator
     h.l model.occurred_at, format: :message
   end
 
+  def display_occurred_at
+    case model.spot_hit_status
+    when 0
+      { css_class: 'pending', css_emoji: 'fas fa-clock' }
+    when 1
+      { css_class: 'delivered', css_emoji: 'fas fa-check' }
+    when 2
+      { css_class: 'sent', css_emoji: 'fas fa-paper-plane' }
+    when 3
+      { css_class: 'in-progress', css_emoji: 'fas fa-sync-alt' }
+    when 4
+      { css_class: 'failed', css_emoji: 'fas fa-times' }
+    else
+      { css_class: 'exipired', css_emoji: 'fas fa-hourglass-end' }
+    end
+  end
+
   def timeline_occurred_at
     h.l model.occurred_at.to_date, format: :message
   end
