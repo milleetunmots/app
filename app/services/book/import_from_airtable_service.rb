0b777ec5fb5109efb560c0e3c5e6f606b5d5408b
aristidebamenou <aristide.bamenou@hotmail.com>
2024-09-30 10:00:22 +0200

    feature(book): update cover

--- app/services/book/import_from_airtable_service.rb
+++ app/services/book/import_from_airtable_service.rb
@@ -1,7 +1,7 @@
 class Book::ImportFromAirtableService
 
   def initialize
-    @airtable_books = Airtables::Book.all.map { |book| { ean: book['EAN'], title: book['Titre du livre'], cover: book['Photo de la couverture'], modules: book['Modules'] } }
+    @airtable_books = Airtables::Book.all.map { |book| { ean: book['EAN'], title: book['Titre du livre'], cover: book['Photo de la couverture'].first, modules: book['Modules'] } }
   end
 
   def call
@@ -9,6 +9,7 @@ class Book::ImportFromAirtableService
       @to_save = false
       @ean = airtable_book[:ean]
       @title = airtable_book[:title]
+      @cover = airtable_book[:cover]
       @support_module_ids = airtable_book[:modules].map do |module_id|
         airtable_module = Airtables::Module.find(module_id)
         SupportModule.where(name: airtable_module['titre'].strip, age_ranges: [airtable_module.ages]).first.id
@@ -17,6 +18,7 @@ class Book::ImportFromAirtableService
       import_new_book
       update_title
       update_support_modules
+      update_cover
       @book.save! if @to_save
     end
     self
@@ -35,6 +37,31 @@ class Book::ImportFromAirtableService
 
     @to_save = true
     @book.title = @title
+  end
+
+  def download_cover
+    save_dir = Rails.root.join('tmp', 'images')
+    FileUtils.mkdir_p(save_dir) unless Dir.exist?(save_dir)
+
+    file_path = File.join(save_dir, @cover['filename'])
+    URI.open(@cover['url']) do |image|
+      File.open(file_path, 'wb') do |file|
+        file.write(image.read)
+      end
+    end
+    file_path
+  end
+
+  def update_cover
+    return if @book.media&.name == @cover['filename']
+
+    @to_save = true
+    @book.media = Media::Image.new(name: @cover['filename'])
+    @book.media.file.attach(
+      io: File.open(download_cover),
+      filename: @cover['filename'],
+      content_type: @cover['type']
+    )
   end
 
   def update_support_modules
