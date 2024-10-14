1f7af196c2c4d835393de69914737276eaf8ae8d
aristidebamenou <aristide.bamenou@hotmail.com>
2024-10-14 17:15:49 +0200

    feature(call0) : send call0 goals reminder messages service

--- app/jobs/child_support/assign_default_call_status_job.rb
+++ app/jobs/child_support/assign_default_call_status_job.rb
@@ -3,6 +3,10 @@ class ChildSupport
     class AssignDefaultCallStatusJob < ApplicationJob
       def perform(group_id, call_number)
         ChildSupport::AssignDefaultCallStatusService.new(group_id, call_number).call
+        if call_number == 1
+          service = ChildSupport::SendCall0GoalsReminderMessagesService.new(group_id).call
+          Rollbar.error(service.errors) if service.errors.any?
+        end
         return unless call_number == 3
 
         service = ChildSupport::SendCallGoalsMessagesService.new(group_id, 3).call
