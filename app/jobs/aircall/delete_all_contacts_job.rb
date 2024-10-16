f5019561d8255d1478906151cda9bcf8fdd61709
Maxime Martin <dev@maxmartin.fr>
2024-10-16 12:46:27 +0200

    fix(aircall) : delete all contacts jobs args

--- app/jobs/aircall/delete_all_contacts_job.rb
+++ app/jobs/aircall/delete_all_contacts_job.rb
@@ -2,7 +2,7 @@ module Aircall
   class DeleteAllContactsJob < ApplicationJob
 
     def perform
-      service = Aircall::DeleteContactService.new.call
+      service = Aircall::DeleteContactService.new(delete_all: true).call
       Rollbar.error(service.errors) if service.errors.any?
     end
   end
