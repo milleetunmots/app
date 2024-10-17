7ae996d936d82b0ff293e4c3eeee2d2a00a77e8b
Maxime Martin <dev@maxmartin.fr>
2024-10-17 15:22:03 +0200

    feat(workshops) : dont return excluded parents in parents search (#916)

--- app/services/workshop/program_workshop_invitation_service.rb
+++ app/services/workshop/program_workshop_invitation_service.rb
@@ -2,7 +2,7 @@ class Workshop::ProgramWorkshopInvitationService < ProgramMessageService
 
   def format_data_for_spot_hit
     @recipient_data = {}
-    parents = Parent.where(id: @parent_ids).pluck(:id, :security_code)
+    parents = Parent.where(id: @parent_ids, is_excluded_from_workshop: false).pluck(:id, :security_code)
     parents.each do |id, security_code|
       @recipient_data[id.to_s] = {}
       @recipient_data[id.to_s]['RESPONSE_LINK'] = Rails.application.routes.url_helpers.edit_workshop_participation_url(
