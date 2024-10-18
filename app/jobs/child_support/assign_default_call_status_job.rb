ba6c2af520ffd80775abd40e71c00ad3e008bac6
aristidebamenou <aristide.bamenou@hotmail.com>
2024-10-18 10:37:16 +0200

    refactoring(send_call_goals_message_service) : move the service into AssignDefaultCallStatusJob

--- app/jobs/child_support/assign_default_call_status_job.rb
+++ app/jobs/child_support/assign_default_call_status_job.rb
@@ -3,14 +3,18 @@ class ChildSupport
     class AssignDefaultCallStatusJob < ApplicationJob
       def perform(group_id, call_number)
         ChildSupport::AssignDefaultCallStatusService.new(group_id, call_number).call
-        if call_number == 1
-          service = ChildSupport::SendCall0GoalsReminderMessagesService.new(group_id).call
-          Rollbar.error("Send call0 goals reminder messages service errors: #{service.errors}") if service.errors.any?
-        end
-        return unless call_number == 3
 
-        service = ChildSupport::SendCallGoalsMessagesService.new(group_id, 3).call
-        Rollbar.error(service.errors) if service.errors.any?
+        case call_number
+        when 0
+          service = ChildSupport::SendCallGoalsMessagesService.new(group_id, 0).call
+          Rollbar.error("Send call0 first reminder messages service errors : #{service.errors}") if service.errors.any?
+        when 1
+          service = ChildSupport::SendCall0GoalsReminderMessagesService.new(group_id).call
+          Rollbar.error("Send call0 goals second reminder messages service errors: #{service.errors}") if service.errors.any?
+        when 3
+          service = ChildSupport::SendCallGoalsMessagesService.new(group_id, 3).call
+          Rollbar.error(service.errors) if service.errors.any?
+        end
       end
     end
 end
--- app/jobs/group/program_sms_to_bilinguals_job.rb
+++ app/jobs/group/program_sms_to_bilinguals_job.rb
@@ -4,8 +4,5 @@ class Group::ProgramSmsToBilingualsJob < ApplicationJob
 
   def perform(group_id, first_sms_date)
     Group::ProgramSmsToBilingualsService.new(group_id, first_sms_date).call
-
-    service = ChildSupport::SendCallGoalsMessagesService.new(group_id, 0).call
-    Rollbar.error(service.errors) if service.errors.any?
   end
 end
