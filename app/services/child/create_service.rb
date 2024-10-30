ecd60b52e3881800d90457aa1fb393e678e127db
Maxime Martin <dev@maxmartin.fr>
2024-10-30 10:45:00 +0100

    feat(children) : build children with children_source instead of creating them after the save (#923)

--- app/services/child/create_service.rb
+++ app/services/child/create_service.rb
@@ -27,14 +27,8 @@ class Child
       build_siblings
       detect_errors
       if @child.errors.empty? && @child.save
-        ChildrenSource.create(@children_source_attributes.merge(child_id: @child.id))
         send_form_by_sms
         send_not_supported_sms
-        @child.siblings.each do |sibling|
-          next if sibling.id.eql?(@child.id)
-
-          ChildrenSource.create(@children_source_attributes.merge(child_id: sibling.id))
-        end
         create_parent_registration
         @child_under_four_months = @child.siblings.any? { |child| child.months < 4 }
       end
@@ -97,6 +91,7 @@ class Child
     end
 
     def build
+      @attributes.merge!(children_source_attributes: @children_source_attributes)
       parent1_attributes = @parent1_attributes.merge(parent1_present? ? @parent1_attributes : @parent2_attributes).merge(tag_list: @attributes[:tag_list])
       parent2_attributes = @parent1_attributes.merge(@parent2_attributes).merge(tag_list: @attributes[:tag_list]) if parent2_present? && parent1_present?
       @child = if parent2_attributes.nil?
@@ -119,6 +114,7 @@ class Child
         attributes[:should_contact_parent2] = @child.should_contact_parent2
         attributes[:child_support] = @child.child_support
         attributes[:tag_list] = @child.tag_list
+        attributes[:children_source_attributes] = @children_source_attributes
         next unless @registration_origin == 4
 
         attributes[:group_status] = @child.group_status
