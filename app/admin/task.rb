d2d27d645684d38f0fef19b1a0452bba5e95ab71
aristidebamenou <aristide.bamenou@hotmail.com>
2024-11-08 15:01:04 +0100

    fix(task) : show scope

--- app/admin/task.rb
+++ app/admin/task.rb
@@ -49,12 +49,10 @@ ActiveAdmin.register Task do
   end
 
   scope(:caller_task, default: proc { current_admin_user.caller? }, group: :reported) { |scope| scope.caller_task(current_admin_user) }
-  unless proc { current_admin_user.caller? }
-    scope(:mine, default: proc { !current_admin_user.caller? }, group: :assignee) { |scope| scope.todo.assigned_to(current_admin_user) }
-    scope :all, group: :assignee
-    scope :todo
-    scope :done
-  end
+  scope(:mine, default: proc { !current_admin_user.caller? }, group: :assignee) { |scope| scope.todo.assigned_to(current_admin_user) }
+  scope :all, group: :assignee
+  scope :todo
+  scope :done
 
   filter :title
   filter :assignee,
