51eb87d1464707e1ba4ce531bda80aec178e523c
Maxime Martin <dev@maxmartin.fr>
2024-12-04 12:21:55 +0100

    feat(books) : schedule save_book_from_support_module_service at appropriate time

--- app/services/children_support_module/save_book_from_support_module_service.rb
+++ app/services/children_support_module/save_book_from_support_module_service.rb
@@ -1,19 +1,21 @@
 class ChildrenSupportModule
   class SaveBookFromSupportModuleService
+		# Support module has books associated, but these can change over time
+		# So we save the book associated to a chosen module on the corresponding CSM before we send them
 
-		def initialize(group_id:, module_index:)
+		def initialize(group_id:)
 			@group_id = group_id
-			@module_index = module_index
     end
 
     def call
-			group = Group.find(group_id)
-			children_ids = group.children.ids
-			# get not_programmed + with_support_module CSM for the group
+			# get not_programmed + with_support_module CSM for the group (only parent1 choices)
 			chosen_modules = ChildrenSupportModule.chosen_modules_for_group(@group_id)
 			chosen_modules = chosen_modules.uniq { |csm| [csm.child_id, csm.parent_id] }
+			# keep CSM of active children only
+			chosen_modules_ids = ChildrenSupportModule.joins(:child)
+													 .where(id: chosen_modules)
+													 .where(children: { group_status: 'active' }).ids.join(',')
 			# SQL query because AR update_call can't handle joins
-			chosen_modules_ids = chosen_modules.pluck(:id).join(',')
 			ActiveRecord::Base.connection.execute(<<-SQL)
 				UPDATE children_support_modules
 				SET book_id = support_modules.book_id
