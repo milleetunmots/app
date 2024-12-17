f99b457d75057375702fc3fa4d0e44e08e910555
aristidebamenou <aristide.bamenou@hotmail.com>
2024-12-17 11:35:28 +0100

    feature(xp_parents) : rspec fixed

--- spec/requests/children_spec.rb
+++ spec/requests/children_spec.rb
@@ -103,7 +103,7 @@ RSpec.describe ChildrenController, type: :request do
       end
 
       it "redirects to created page with right sms_url_form" do
-        expect(response).to redirect_to(created_child_path(sms_url_form: "#{ENV['TYPEFORM_URL']}#child_support_id=#{Child.last.child_support.id}", child_under_four_months: birthdate > 4.months.ago))
+        expect(response).to redirect_to(created_child_path(sms_url_form: "#{ENV['TYPEFORM_URL']}#child_support_id=#{Child.last.child_support.id}", child_under_four_months: birthdate > 4.months.ago, youngest_child_under_twenty_four_months: birthdate > 24.months.ago))
       end
     end
 
