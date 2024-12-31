e5b1c451646a29f7b66602a4bc7ebd34ed01e3cf
Maxime Martin <dev@maxmartin.fr>
2024-12-31 13:33:17 +0100

    feat(aircall) : setup AircallCall and AircallMessage

---
+++ app/models/aircall_message.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: aircall_messages
+#
+#  id               :bigint           not null, primary key
+#  body             :text
+#  direction        :string
+#  sent_at          :datetime
+#  status           :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  aircall_id       :string
+#  caller_id        :bigint           not null
+#  child_support_id :bigint
+#  parent_id        :bigint
+#
+# Indexes
+#
+#  index_aircall_messages_on_aircall_id        (aircall_id)
+#  index_aircall_messages_on_caller_id         (caller_id)
+#  index_aircall_messages_on_child_support_id  (child_support_id)
+#  index_aircall_messages_on_parent_id         (parent_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (caller_id => admin_users.id)
+#
+class AircallMessage < ApplicationRecord
+	belongs_to :child_support
+	belongs_to :parent
+	belongs_to :caller, class_name: 'AdminUser'
+
+	validates :aircall_id, presence: true
+	validates :direction, inclusion: { in: %w[inbound outbound] }
+	validates :status, inclusion: { in: %w[sent received pending] }
+end
