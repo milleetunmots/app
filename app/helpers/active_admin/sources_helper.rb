34b5b0763dd7159ecef9eb976b0ba6ec656f9651
Maxime Martin <dev@maxmartin.fr>
2025-01-14 09:34:24 +0100

    feat(sources) : add is_archived, dont allow archived source in registration page (#970)

--- app/helpers/active_admin/sources_helper.rb
+++ app/helpers/active_admin/sources_helper.rb
@@ -2,23 +2,23 @@ module ActiveAdmin::SourcesHelper
 
   def source_select_for_pmi(dpt = nil)
     if dpt.nil?
-      Source.by_pmi.map { |pmi| [pmi.decorate.name, pmi.id] }
+      Source.active.by_pmi.map { |pmi| [pmi.decorate.name, pmi.id] }
     else
-      Source.by_pmi.where(department: dpt).map { |pmi| [pmi.decorate.name, pmi.id] }
+      Source.active.by_pmi.where(department: dpt).map { |pmi| [pmi.decorate.name, pmi.id] }
     end
   end
 
   def source_select_for_caf
-    Source.by_caf.map { |caf| [caf.name, caf.id] }
+    Source.active.by_caf.map { |caf| [caf.name, caf.id] }
   end
 
   def source_select_for_bao
-    Source.by_bao.map { |bao| [bao.name, bao.id] }
+    Source.active.by_bao.map { |bao| [bao.name, bao.id] }
   end
 
   def source_select_for_local_partner
-    sources_without_other = Source.by_local_partner.reject { |local_partner| local_partner.name == 'Autre' }
-    other = Source.by_local_partner.select { |local_partner| local_partner.name == 'Autre' }
+    sources_without_other = Source.active.by_local_partner.reject { |local_partner| local_partner.name == 'Autre' }
+    other = Source.active.by_local_partner.select { |local_partner| local_partner.name == 'Autre' }
 
     (sources_without_other + other).map { |local_partner| [local_partner.name, local_partner.id] }
   end
@@ -29,6 +29,10 @@ module ActiveAdmin::SourcesHelper
 
   def source_select_collection
     Source.order(channel: :desc, department: :asc, name: :asc).map { |source| [source.decorate.name, source.id] }
+  end
+
+  def source_active_select_collection
+    Source.active.order(channel: :desc, department: :asc, name: :asc).map { |source| [source.decorate.name, source.id] }
   end
 
   def source_details_suggestions
