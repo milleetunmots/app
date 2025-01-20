9d6c481340aaa17ee659cf0a6ea989f2a013a95e
aristidebamenou <aristide.bamenou@hotmail.com>
2025-01-20 15:30:04 +0100

    feat(tags_helper) : tag_name_collection returns only tag visible by callers if the current admin user is caller

--- app/helpers/active_admin/tags_helper.rb
+++ app/helpers/active_admin/tags_helper.rb
@@ -1,7 +1,11 @@
 module ActiveAdmin::TagsHelper
 
-  def tag_name_collection
-    ActsAsTaggableOn::Tag.order("LOWER(name)").pluck(:name)
+  def tag_name_collection(current_admin_user_is_caller)
+    if current_admin_user_is_caller
+      ActsAsTaggableOn::Tag.where(is_visible_by_callers: current_admin_user_is_caller).order("LOWER(name)").pluck(:name)
+    else
+      ActsAsTaggableOn::Tag.order("LOWER(name)").pluck(:name)
+    end
   end
 
   def module_name_collection
@@ -21,7 +25,7 @@ module ActiveAdmin::TagsHelper
     form.input context_list.to_sym, {
         multiple: true,
         label: "Tags",
-        collection: tag_name_collection,
+        collection: tag_name_collection(false),
         input_html: input_html
       }.deep_merge(options)
   end
