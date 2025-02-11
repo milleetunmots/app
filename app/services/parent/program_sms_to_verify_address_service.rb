05219beaa0073a89efc6db1fc61f6358ce1b8ee9
aristidebamenou <aristide.bamenou@hotmail.com>
2025-02-11 15:36:16 +0100

    feature(program_sms_to_verify_address_service) : call

--- app/services/parent/program_sms_to_verify_address_service.rb
+++ app/services/parent/program_sms_to_verify_address_service.rb
@@ -1,30 +1,30 @@
-class Parent::ProgramSmsToVerifyAdressService
+class Parent::ProgramSmsToVerifyAddressService
 
-  MESSAGE = "1001mots : Boujour, nous allons bientôt envoyer un livre pour {PRENOM_ENFANT}. Nous allons l'envoyer à l'adresse suivante : {ADDRESS}. Si l'adresse postale ou le nom sur la boîte aux lettre ne sont pas bons, merci de les modifier ici :".freeze
+  MESSAGE = "1001mots : Boujour, nous allons bientôt envoyer un livre pour {PRENOM_ENFANT}. Nous allons l'envoyer à l'adresse suivante : {ADDRESS}Si l'adresse postale ou le nom sur la boîte aux lettre ne sont pas bons, merci de les modifier ici :".freeze
 
   attr_reader :errors
 
   def initialize(group_id)
     @message = "#{MESSAGE} https://form.typeform.com/to/VpPCzGfD#parent_id=xxxxx&security_code=xxxxx"
     @errors = []
+    @children = Group.find(group_id).children.where(group_status: 'active', should_contact_parent1: true)
     @date = Time.zone.now
   end
 
   def call
+    @children.find_each do |child|
+      @parent = child.parent1
+      @message = MESSAGE.gsub('{ADDRESS}', "\n#{@parent.address}\n#{@parent.letterbox_name}\n#{@parent.postal_code} #{@parent.city_name}\n")
+      @message = @message.gsub('parent_id=xxxxx', "parent_id=#{@parent.id}")
+      @message = @message.gsub('security_code=xxxxx', "security_code=#{@parent.security_code}")
+      service = ProgramMessageService.new(
+        @date.strftime('%d-%m-%Y'),
+        @date.strftime('%H:%M'),
+        ["parent.#{@parent.id}"],
+        @message
+      ).call
+      @errors << { service: 'ProgramMessageService', parent_id: @parent.id, parent_phone_number: @parent.phone_number, errors: service.errors } if service.errors.any?
+    end
     self
-  end
-
-  private
-
-  def send_verification_message
-    @message = @message.gsub('parent_id=xxxxx', "parent_id=#{@parent.id}")
-    @message = @message.gsub('security_code=xxxxx', "security_code=#{@parent.security_code}")
-    service = ProgramMessageService.new(
-      @date.strftime('%d-%m-%Y'),
-      @date.strftime('%H:%M'),
-      ["parent.#{@parent.id}"],
-      @message
-    ).call
-    @errors << "Address Verification message not sent to #{@parent.first_name} #{@parent.last_name} : #{service.errors.join(' - ')}" if service.errors.any?
   end
 end
