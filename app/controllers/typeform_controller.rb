1dd33ca8a669c679554b584a0b1e4f5517353554
aristidebamenou <aristide.bamenou@hotmail.com>
2025-02-20 10:02:23 +0100

    feature(update_address) : add upstream address updating typeform

--- app/controllers/typeform_controller.rb
+++ app/controllers/typeform_controller.rb
@@ -8,13 +8,15 @@ class TypeformController < ApplicationController
   CALL_THREE_OBSERVING_TYPEFORM_ID = ENV['CALL_THREE_OBSERVING_TYPEFORM_ID'].freeze
   INITIAL_TYPEFORM_ID = ENV['INITIAL_TYPEFORM_ID'].freeze
   UPDATING_ADDRESS_TYPEFORM_ID = ENV['UPDATING_ADDRESS_TYPEFORM_ID'].freeze
+  UPSTREAM_ADDRESS_UPDATING_TYPEFORM_ID = ENV['UPSTREAM_ADDRESS_UPDATING_TYPEFORM_ID'].freeze
   TYPEFORM_IDS = [
     MIDWAY_TYPEFORM_ID,
     CALL_ZERO_GOALS_TYPEFORM_ID,
     CALL_THREE_SPEAKING_TYPEFORM_ID,
     CALL_THREE_OBSERVING_TYPEFORM_ID,
     INITIAL_TYPEFORM_ID,
-    UPDATING_ADDRESS_TYPEFORM_ID
+    UPDATING_ADDRESS_TYPEFORM_ID,
+    UPSTREAM_ADDRESS_UPDATING_TYPEFORM_ID
   ].freeze
 
   def webhooks
@@ -29,7 +31,7 @@ class TypeformController < ApplicationController
           Typeform::CallGoalsFormService.new(params[:form_response], 3).call
         when INITIAL_TYPEFORM_ID
           Typeform::InitialFormService.new(params[:form_response]).call
-        when UPDATING_ADDRESS_TYPEFORM_ID
+        when UPDATING_ADDRESS_TYPEFORM_ID, UPSTREAM_ADDRESS_UPDATING_TYPEFORM_ID
           Typeform::UpdateAddressService.new(params[:form_response]).call
         end
       Rollbar.error('Typeform service error', service: service.class.to_s, errors: service.errors, form_response: params[:form_response]) unless service.errors.empty?
--- app/services/typeform/update_address_service.rb
+++ app/services/typeform/update_address_service.rb
@@ -1,12 +1,24 @@
 module Typeform
   class UpdateAddressService < Typeform::TypeformService
     FIELD_IDS = {
-      address: ENV['ADDRESS_TYPEFORM_ADDRESS_ID'],
-      city_name: ENV['ADDRESS_TYPEFORM_CITY_NAME_ID'],
-      postal_code: ENV['ADDRESS_TYPEFORM_POSTAL_CODE_ID'],
-      letterbox_name: ENV['ADDRESS_TYPEFORM_LETTERBOX_NAME_ID']
+      ENV['UPDATING_ADDRESS_TYPEFORM_ID'] => {
+        address: ENV['ADDRESS_TYPEFORM_ADDRESS_ID'],
+        city_name: ENV['ADDRESS_TYPEFORM_CITY_NAME_ID'],
+        postal_code: ENV['ADDRESS_TYPEFORM_POSTAL_CODE_ID'],
+        letterbox_name: ENV['ADDRESS_TYPEFORM_LETTERBOX_NAME_ID']
+      },
+      ENV['UPSTREAM_ADDRESS_UPDATING_TYPEFORM_ID'] => {
+        address: ENV['UPSTREAM_ADDRESS_UPDATING_ADDRESS_ID'],
+        city_name: ENV['UPSTREAM_ADDRESS_UPDATING_CITY_NAME_ID'],
+        postal_code: ENV['UPSTREAM_ADDRESS_UPDATING_POSTAL_CODE_ID'],
+        letterbox_name: ENV['UPSTREAM_ADDRESS_UPDATING_LETTERBOX_NAME_ID']
+      }
     }.freeze
 
+    def initialize(form_responses)
+      super(form_responses)
+      @form_id = @form_responses[:form_id]
+    end
 
     def call
       verify_hidden_variable('parent_id')
@@ -21,13 +33,13 @@ module Typeform
 
       @answers.each do |answer|
         case answer[:field][:id]
-        when FIELD_IDS[:address]
+        when FIELD_IDS[@form_id][:address]
           @parent.address = answer[:text]
-        when FIELD_IDS[:city_name]
+        when FIELD_IDS[@form_id][:city_name]
           @parent.city_name = answer[:text]
-        when FIELD_IDS[:postal_code]
+        when FIELD_IDS[@form_id][:postal_code]
           @parent.postal_code = answer[:number]
-        when FIELD_IDS[:letterbox_name]
+        when FIELD_IDS[@form_id][:letterbox_name]
           @parent.letterbox_name = answer[:text]
         end
       end
