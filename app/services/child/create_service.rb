b09e24edb84ce0526d38ee7884e9d4e4b270209f
aristidebamenou <aristide.bamenou@hotmail.com>
2025-02-27 13:17:45 +0100

    fix(child_create_service) : update SMS sending logic to include CAF subscription check

--- app/services/child/create_service.rb
+++ app/services/child/create_service.rb
@@ -138,12 +138,11 @@ class Child
 
     def send_form_by_sms
       return if 'filtre-diplome-KO'.in? @child.tag_list
-      return if @registration_origin == 2 && ENV['CAF_SUBSCRIPTION'].present?
 
       @sms_url_form = "#{ENV.fetch('TYPEFORM_URL', nil)}#child_support_id=#{@child.child_support.id}"
       message = "1001mots: Bonjour ! Je suis ravie de votre inscription à notre accompagnement ! Si vous avez 3 minutes, merci de répondre à ce court questionnaire #{@sms_url_form}"
 
-      SpotHit::SendSmsService.new([@child.parent1_id], Time.zone.now.to_i, message).call if @registration_origin == 4
+      SpotHit::SendSmsService.new([@child.parent1_id], Time.zone.now.to_i, message).call if @registration_origin == 4 || (@registration_origin == 2 && ENV['CAF_SUBSCRIPTION'].nil?)
       SpotHit::SendSmsService.new([@child.parent1_id], Time.zone.now.change({ hour: 18 }).to_i, message).call if @registration_origin.in?([3, 5])
     end
 
