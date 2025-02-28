a1c7a46d2c1bb5ca1932943df715524bfd96d3d2
Maxime Martin <dev@maxmartin.fr>
2025-02-28 12:02:52 +0100

    fix(tags) : display all tags in tag_list form to avoid tags silent tag deletion

--- app/helpers/active_admin/tags_helper.rb
+++ app/helpers/active_admin/tags_helper.rb
@@ -25,7 +25,7 @@ module ActiveAdmin::TagsHelper
     form.input context_list.to_sym, {
       multiple: true,
       label: 'Tags',
-      collection: tag_name_collection(current_admin_user.caller?),
+      collection: tag_name_collection(false),
       input_html: input_html
     }.deep_merge(options)
   end
