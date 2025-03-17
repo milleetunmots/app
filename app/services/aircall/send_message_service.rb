8050d75a510ec3a6c086d5632d4df5da6cee5e8a
Maxime Martin <dev@maxmartin.fr>
2025-03-17 11:03:41 +0100

    feat(aircall) : send_message_service

---
+++ app/services/aircall/send_message_service.rb
@@ -0,0 +1,20 @@
+module Aircall
+  class SendMessageService < Aircall::ApiBase
+    attr_reader :errors
+
+    def initialize(number_id:, to:, body:)
+      @errors = []
+      @number_id = number_id
+      @to = to
+      @body = body
+    end
+
+    def call
+      return self unless ENV['AIRCALL_ENABLED']
+
+      response = http_client_with_auth.post(build_url(NUMBERS_ENDPOINT, "/#{@number_id}/messages/native/send"), json: { to: @to, body: @body })
+      @errors << { message: "L'envoi du message Aircall a échoué : #{response.status.reason}", status: response.status.to_i } unless response.status.success?
+      self
+    end
+  end
+end
