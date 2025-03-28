6abfed05fdf44a1eeca41ee0c3a614e93e3f5536
aristidebamenou <aristide.bamenou@hotmail.com>
2025-03-28 11:48:12 +0100

    fix(check_address) : update message

--- app/services/parent/check_address_reminder_service.rb
+++ app/services/parent/check_address_reminder_service.rb
@@ -12,25 +12,25 @@ class Parent::CheckAddressReminderService
 
   def call
     ChildSupport.joins(:children).where.not(address_suspected_invalid_at: nil).where(children: { group_status: 'active' }).find_each do |child_support|
-      @parent = child_support.parent1
-      next if @parent.message_already_sent?(6.days.ago, Parent::CheckAddressService::MESSAGE)
+      parent = child_support.parent1
+      next if parent.message_already_sent?(6.days.ago, Parent::CheckAddressService::MESSAGE)
 
-      send_verification_message
+      send_verification_message(parent)
     end
     self
   end
 
   private
 
-  def send_verification_message
-    @message = @message.gsub('parent_id=xxxxx', "parent_id=#{@parent.id}")
-    @message = @message.gsub('security_code=xxxxx', "security_code=#{@parent.security_code}")
+  def send_verification_message(parent)
+    message = @message.gsub('parent_id=xxxxx', "parent_id=#{parent.id}")
+    message = message.gsub('security_code=xxxxx', "security_code=#{parent.security_code}")
     service = ProgramMessageService.new(
       @date.strftime('%d-%m-%Y'),
       @date.strftime('%H:%M'),
-      ["parent.#{@parent.id}"],
-      @message
+      ["parent.#{parent.id}"],
+      message
     ).call
-    @errors << "Address Verification message reminder not sent to #{@parent.first_name} #{@parent.last_name} : #{service.errors.join(' - ')}" if service.errors.any?
+    @errors << "Address Verification message reminder not sent to #{parent.first_name} #{parent.last_name} : #{service.errors.join(' - ')}" if service.errors.any?
   end
 end
