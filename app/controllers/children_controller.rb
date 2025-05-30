0187241bd3d8abece0415cc6f32e9b3706bb7ee3
aristidebamenou <52001075+aristidebamenou@users.noreply.github.com>
2025-05-30 15:02:56 +0200

    Merge pull request #1070 from milleetunmots/feature/child_registration
    Changer le wording validation inscription + le sms post inscription

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -69,12 +69,14 @@ class ChildrenController < ApplicationController
 
     case current_registration_origin
     when 5
-      @message = I18n.t('inscription_success.pro')
+      @title = I18n.t('inscription_success.pro_title')
+      @message = I18n.t('inscription_success.pro_local_partners')
       @again = true
       @widget = false
       @new_link = new_local_partner_registration_path
     when 4
       @widget = false
+      @title = I18n.t('inscription_success.title')
       if params[:parent1]
         @again = false
         @with_parent_no_target = true
@@ -87,11 +89,13 @@ class ChildrenController < ApplicationController
     when 3
       # for this form we keep the registration_origin
       # so that multiple children can be registered
-      @message = I18n.t('inscription_success.pro')
+      @title = I18n.t('inscription_success.pro_title')
+      @message = I18n.t('inscription_success.pro_pmi')
       @again = true
       @widget = false
       @new_link = new_pmi_registration_path
     when 2
+      @title = I18n.t('inscription_success.title')
       if ENV['CAF_SUBSCRIPTION'].present?
         @child_id = params[:caf_subscripted_child_id]
         set_caf_subscription_form_variables
@@ -106,6 +110,7 @@ class ChildrenController < ApplicationController
         @new_link = new_caf_registration_path
       end
     else
+      @title = I18n.t('inscription_success.title')
       @message = I18n.t('inscription_success.with_widget')
       @again = false
       @widget = true
