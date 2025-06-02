2041bf17c9d4ac89a7fc49bee72ad40aa14e1563
aristidebamenou <aristide.bamenou@hotmail.com>
2025-06-02 17:08:52 +0200

    fix(VerifyChosenModulesTaskJob) : Update scopes to filter by active group status

--- app/jobs/children_support_module/verify_chosen_modules_task_job.rb
+++ app/jobs/children_support_module/verify_chosen_modules_task_job.rb
@@ -4,9 +4,9 @@ class ChildrenSupportModule
       group = Group.includes(children: :children_support_modules).find(group_id)
       logistics_team_members = AdminUser.all_logistics_team_members
       missing_support_modules = ChildrenSupportModule.where(support_module: nil, child_id: group.children.where(group_status: 'active').ids)
-      description_text = ActionController::Base.helpers.link_to('Compléter les modules sans choix', Rails.application.routes.url_helpers.admin_children_support_modules_url(scope: 'without_choice', q: { active_group_id_in: [group_id] }), target: '_blank', class: 'blue')
+      description_text = ActionController::Base.helpers.link_to('Compléter les modules sans choix', Rails.application.routes.url_helpers.admin_children_support_modules_url(scope: 'without_choice', q: { group_id_in: [group_id] }), target: '_blank', class: 'blue')
       description_text << ' - '
-      description_text << ActionController::Base.helpers.link_to('Compléter les modules "laisse le choix à 1001mots"', Rails.application.routes.url_helpers.admin_children_support_modules_url(scope: 'with_the_choice_to_make_by_us', q: { active_group_id_in: [group_id] }), target: '_blank', class: 'blue')
+      description_text << ActionController::Base.helpers.link_to('Compléter les modules "laisse le choix à 1001mots"', Rails.application.routes.url_helpers.admin_children_support_modules_url(scope: 'with_the_choice_to_make_by_us', q: { group_id_in: [group_id] }), target: '_blank', class: 'blue')
       return unless missing_support_modules.any?
 
       Task::CreateAutomaticTaskService.new(
