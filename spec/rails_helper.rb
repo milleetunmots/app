3c5852a48735b52fda278d810637627ad2a69646
aristidebamenou <aristide.bamenou@hotmail.com>
2025-06-19 14:49:15 +0200

    feat(rails_helper): Enhance SimpleCov configuration by adding Services group and filtering admin files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,9 @@
 require "simplecov"
 
-SimpleCov.start "rails"
+SimpleCov.start "rails" do
+  add_group "Services", "app/services"
+  add_filter "app/admin/"
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
