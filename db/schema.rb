c7507dec80be47bbb5bc96eacee5f0079c67fd12
aristidebamenou <aristide.bamenou@hotmail.com>
2025-06-24 18:29:53 +0200

    feat(group): Add support_module_sent_dates to group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_05_06_151505) do
+ActiveRecord::Schema.define(version: 2025_06_24_144417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -508,6 +508,7 @@ ActiveRecord::Schema.define(version: 2025_05_06_151505) do
     t.date "call3_end_date"
     t.boolean "is_excluded_from_analytics", default: false, null: false
     t.boolean "enable_calls_recording", default: false, null: false
+    t.jsonb "support_module_sent_dates"
     t.index ["discarded_at"], name: "index_groups_on_discarded_at"
     t.index ["ended_at"], name: "index_groups_on_ended_at"
     t.index ["started_at"], name: "index_groups_on_started_at"
