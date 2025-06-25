617d4a7f5e7b922ecb3854056c92d60b54c78804
aristidebamenou <52001075+aristidebamenou@users.noreply.github.com>
2025-06-25 12:30:17 +0200

    Remplacement du terme “Accompagnantes” dans la fonctionnalité d’attribution automatique des accompagnantes (#1088)
    * fix(Airtables): Replace hardcoded keys and table names with environment variables
    
    * fix(Airtables): Replace 'Accompagnante' by 'Appelante'

--- app/models/airtables/caller.rb
+++ app/models/airtables/caller.rb
@@ -1,11 +1,11 @@
 class Airtables::Caller < Airrecord::Table
 
-  self.base_key = 'appDlEdpmapLFJ6B9'.freeze
-  self.table_name = 'tblHAspFrqJq9u9hU'.freeze
+  self.base_key = ENV['AIRTABLE_CALLER_BASE_KEY'].freeze
+  self.table_name = ENV['AIRTABLE_CALLER_TABLE_NAME'].freeze
 
-  has_many :call_missions, class: "Airtables::CallMission", column: "Missions d'appels"
+  has_many :call_missions, class: 'Airtables::CallMission', column: "Missions d'appels"
 
   def self.caller_id_by_airtable_caller_id(airtable_caller_id)
-    find(airtable_caller_id)["N° suivi base"]
+    find(airtable_caller_id)['N° suivi base']
   end
 end
