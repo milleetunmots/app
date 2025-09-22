6a7054b2c790234c41e98df3db6261ed1de2bcc4
Maxime Martin <dev@maxmartin.fr>
2025-09-22 16:38:49 +0200

    refactor : AddDisengagementTagService

--- app/services/group/add_disengagement_tag_service.rb
+++ app/services/group/add_disengagement_tag_service.rb
@@ -1,22 +1,24 @@
 class Group::AddDisengagementTagService
 	attr_reader
 
+  DISENGAGEMENT_STATUSES = ['KO', 'Ne pas appeler', 'Numéro erroné'].freeze
+
 	def initialize(group_id, call_index)
-    # Calculer le nombre d'appels avec KO ou ne pas appeler et recuperer les fiches de suivi avec plus de 2
+    # Récupérer les fiches de suivi avec au moins 2 appels avec KO / Ne pas appeler / Numéro erroné
     @child_supports =
       case call_index
       when 1
         ChildSupport.group_id_in(group_id).with_a_child_in_active_group.
-          where('call1_status IN (?, ?)', 'KO', 'Ne pas appeler').
-          select { |child_support| child_support.call0_status.in? ['KO', 'Ne pas appeler'] }
+          where('call1_status IN (?, ?, ?)', 'KO', 'Ne pas appeler', 'Numéro erroné').
+          select { |child_support| child_support.call0_status.in? DISENGAGEMENT_STATUSES }
       when 2
         ChildSupport.group_id_in(group_id).with_a_child_in_active_group.
-          where('call2_status IN (?, ?)', 'KO', 'Ne pas appeler').
-          select { |child| child.call1_status.in?(['KO', 'Ne pas appeler']) || child.call0_status.in?(['KO', 'Ne pas appeler']) }
+          where('call2_status IN (?, ?, ?)', 'KO', 'Ne pas appeler', 'Numéro erroné').
+          select { |child| child.call1_status.in?(DISENGAGEMENT_STATUSES) || child.call0_status.in?(DISENGAGEMENT_STATUSES) }
       when 3
         ChildSupport.group_id_in(group_id).with_a_child_in_active_group.
-          where('call3_status IN (?, ?)', 'KO', 'Ne pas appeler').
-          select { |child| child.call2_status.in?(['KO', 'Ne pas appeler']) || child.call1_status.in?(['KO', 'Ne pas appeler']) || child.call0_status.in?(['KO', 'Ne pas appeler']) }
+          where('call3_status IN (?, ?, ?)', 'KO', 'Ne pas appeler', 'Numéro erroné').
+          select { |child| child.call2_status.in?(DISENGAGEMENT_STATUSES) || child.call1_status.in?(DISENGAGEMENT_STATUSES) || child.call0_status.in?(DISENGAGEMENT_STATUSES) }
       else
         []
       end
