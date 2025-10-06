99a105f4c4febf093ec8f9b458795ee9940dcf70
Maxime Martin <dev@maxmartin.fr>
2025-10-06 11:45:29 +0200

    fix specs

--- spec/models/workshop_spec.rb
+++ spec/models/workshop_spec.rb
@@ -98,16 +98,16 @@ RSpec.describe Workshop, type: :model do
     context "create workshop_participation for each parent invited" do
       let!(:group) { FactoryBot.create(:group) }
       let!(:first_excluded_parent) { FactoryBot.create(:parent, is_excluded_from_workshop: true) }
-      let!(:first_parent) { FactoryBot.create(:parent, postal_code: 75018) }
-      let!(:second_parent) { FactoryBot.create(:parent, postal_code: 75018) }
-      let!(:second_excluded_parent) { FactoryBot.create(:parent, is_excluded_from_workshop: true, postal_code: 75018) }
+      let!(:first_parent) { FactoryBot.create(:parent, postal_code: 75020) }
+      let!(:second_parent) { FactoryBot.create(:parent, postal_code: 75020) }
+      let!(:second_excluded_parent) { FactoryBot.create(:parent, is_excluded_from_workshop: true, postal_code: 75020) }
 
       let!(:first_child) { FactoryBot.create(:child, available_for_workshops: true, should_contact_parent1: true, group: group, group_status: 'active') }
       let!(:second_child) { FactoryBot.create(:child, available_for_workshops: true, should_contact_parent1: true, group: group, group_status: 'active', parent1: first_excluded_parent) }
       let!(:third_child) { FactoryBot.create(:child, available_for_workshops: true, should_contact_parent1: true, group: group, group_status: 'active', parent1: first_parent) }
       let!(:fourth_child) { FactoryBot.create(:child, available_for_workshops: true, should_contact_parent1: true, group: group, group_status: 'active', parent1: second_parent) }
       let!(:fifth_child) { FactoryBot.create(:child, available_for_workshops: true, should_contact_parent1: true, group: group, group_status: 'active', parent1: second_excluded_parent) }
-      let!(:workshop) { FactoryBot.create(:workshop, parents: [first_child.parent1, first_excluded_parent ], workshop_land: 'Paris 18 eme') }
+      let!(:workshop) { FactoryBot.create(:workshop, parents: [first_child.parent1, first_excluded_parent ], workshop_land: 'Paris 20 eme') }
 
       it "except parents with is_excluded_from_workshop at true" do
         expect(Events::WorkshopParticipation.exists?(related: first_excluded_parent)).to be_falsey
