b35d57c7643e30878434d56ec12195abee17e3c4
aristidebamenou <aristide.bamenou@hotmail.com>
2025-10-07 17:43:27 +0200

    fix(children_disengagement_service_spec): add supporter association to child supports in tests

--- spec/services/child_support/children_disengagement_service_spec.rb
+++ spec/services/child_support/children_disengagement_service_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ChildSupport::ChildrenDisengagementService do
+  let(:supporter) { FactoryBot.create(:admin_user, user_role: 'caller')}
   let!(:group) { FactoryBot.create(:group) }
   let!(:child_to_conserve) { FactoryBot.create(:child, group: group, group_status: 'active') }
   let!(:disengaged_child) { FactoryBot.create(:child, group: group, group_status: 'active') }
-  let!(:child_to_conserve_child_support) { FactoryBot.create(:child_support, current_child: child_to_conserve) }
-  let!(:disengaged_child_child_support) { FactoryBot.create(:child_support, current_child: disengaged_child, tag_list: ['desengage-2appelsKO']) }
+  let!(:child_to_conserve_child_support) { FactoryBot.create(:child_support, current_child: child_to_conserve, supporter: supporter) }
+  let!(:disengaged_child_child_support) { FactoryBot.create(:child_support, current_child: disengaged_child, tag_list: ['desengage-2appelsKO'], supporter: supporter) }
 
   context "when child support's are tagged 'desengage-2appelsKO'" do
     it "updates the children's group_status to 'disengaged'" do
