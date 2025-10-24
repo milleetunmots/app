7025c46c4e2363d60378d2ab04f3806db53d5591
Maxime Martin <dev@maxmartin.fr>
2025-10-24 13:10:58 +0200

    fix Dockerfile.release

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_10_21_091446) do
-
+ActiveRecord::Schema[7.0].define(version: 2025_10_21_091446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -291,10 +290,10 @@ ActiveRecord::Schema.define(version: 2025_10_21_091446) do
     t.text "call3_goals_tracking"
     t.text "call4_goals_tracking"
     t.text "call5_goals_tracking"
-    t.string "call2_family_progress"
-    t.string "call2_previous_goals_follow_up"
     t.string "parent1_available_support_module_list", array: true
     t.string "parent2_available_support_module_list", array: true
+    t.string "call2_family_progress"
+    t.string "call2_previous_goals_follow_up"
     t.text "call0_parent_actions"
     t.text "call0_language_development"
     t.text "call0_notes"
@@ -312,16 +311,16 @@ ActiveRecord::Schema.define(version: 2025_10_21_091446) do
     t.text "call1_goals_tracking"
     t.string "call1_family_progress"
     t.string "call1_previous_goals_follow_up"
+    t.bigint "module2_chosen_by_parents_id"
+    t.bigint "module3_chosen_by_parents_id"
+    t.bigint "module4_chosen_by_parents_id"
+    t.bigint "module5_chosen_by_parents_id"
     t.text "call0_goals_sms"
     t.text "call1_goals_sms"
     t.text "call2_goals_sms"
     t.text "call3_goals_sms"
     t.text "call4_goals_sms"
     t.text "call5_goals_sms"
-    t.bigint "module2_chosen_by_parents_id"
-    t.bigint "module3_chosen_by_parents_id"
-    t.bigint "module4_chosen_by_parents_id"
-    t.bigint "module5_chosen_by_parents_id"
     t.integer "parent_mid_term_rate"
     t.string "parent_mid_term_reaction"
     t.bigint "module6_chosen_by_parents_id"
@@ -365,7 +364,7 @@ ActiveRecord::Schema.define(version: 2025_10_21_091446) do
     t.string "instagram_user"
     t.bigint "restart_support_caller_id"
     t.text "restart_support_details"
-    t.datetime "restart_support_date"
+    t.datetime "restart_support_date", precision: nil
     t.index ["book_not_received"], name: "index_child_supports_on_book_not_received"
     t.index ["call0_parent_progress"], name: "index_child_supports_on_call0_parent_progress"
     t.index ["call0_reading_frequency"], name: "index_child_supports_on_call0_reading_frequency"
@@ -503,10 +502,6 @@ ActiveRecord::Schema.define(version: 2025_10_21_091446) do
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_field_comments_on_author_id"
     t.index ["related_type", "related_id"], name: "index_field_comments_on_related_type_and_related_id"
-  end
-
-  create_table "foo", id: false, force: :cascade do |t|
-    t.integer "x"
   end
 
   create_table "groups", force: :cascade do |t|
