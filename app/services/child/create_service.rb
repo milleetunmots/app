26a959a34a6891a77cb2905b18ceb1f358a18f9e
aristidebamenou <aristide.bamenou@hotmail.com>
2025-10-28 10:53:07 +0100

    feature(msa_integration): add MSA-specific registration flow

--- app/services/child/create_service.rb
+++ app/services/child/create_service.rb
@@ -62,10 +62,10 @@ class Child
       # add tags for bao / local_partner ?
       @attributes[:tag_list] ||= []
       @attributes[:tag_list] << case @registration_origin
+                                when 6, 2 then 'inscriptioncaf'
                                 when 5 then 'inscription5'
                                 when 4 then 'inscription4'
                                 when 3 then 'inscription3'
-                                when 2 then 'inscriptioncaf'
                                 else 'inscription1'
                                 end
     end
