116df285e472f4bef195d87a897f7b85563300a6
aristidebamenou <aristide.bamenou@hotmail.com>
2025-12-18 00:37:19 +0100

    feat(spot_hit): extend SendSmsService to handle multiple recipient types and add support for RCS fallback messages

--- app/services/spot_hit/send_rcs_service.rb
+++ app/services/spot_hit/send_rcs_service.rb
@@ -4,15 +4,14 @@ class SpotHit::SendRcsService
 
   attr_reader :errors
 
-  def initialize(recipients:, planned_timestamp: Time.zone.now, media_id: nil)
+  def initialize(recipients:, planned_timestamp: Time.zone.now, media_id: nil, fallback_message: nil)
     @recipients = recipients
     @planned_timestamp = planned_timestamp
     @form = {
       'key' => ENV['SPOT_HIT_API_KEY'],
       'agent_id' => ENV['SPOT_HIT_AGENT_ID'],
       'media_id' => media_id,
-      'fallback_message' => Media::TextMessagesBundle.find_body_by_rcs_media_id(media_id),
-      'custom_list[]' => @recipients
+      'fallback_message' => fallback_message
     }
     @errors = []
   end
@@ -24,13 +23,18 @@ class SpotHit::SendRcsService
   protected
 
   def send_rcs
-    @form['date'] = Time.zone.now if @planned_timestamp.past?
-    response = HTTP.post(URL, form: @form.merge({'date' => @planned_timestamp.past? ? (Time.zone.now + 1.minute).strftime('%Y-%m-%d %H:%M:%S') : @planned_timestamp.now.strftime('%Y-%m-%d %H:%M:%S')}))
+    @form['date'] = Time.zone.now if Time.zone.at(@planned_timestamp).past?
+    response = HTTP.post(URL, form: @form.merge({ 'date' => Time.zone.at(@planned_timestamp).past? ? (Time.zone.now + 1.minute).strftime('%Y-%m-%d %H:%M:%S') : Time.zone.at(@planned_timestamp).strftime('%Y-%m-%d %H:%M:%S') }))
     response = JSON.parse(response.body.to_s)
     if response['success']
-      create_events(response['id'])
+      # create_events(response['id'])
+      p response['id']
     else
       @errors << "Erreur lors de la programmation de la campagne : #{response['error']['message']}]"
     end
   end
+
+  # def create_events(rcs_id)
+  #
+  # end
 end
--- app/services/support_module/program_service.rb
+++ app/services/support_module/program_service.rb
@@ -24,18 +24,16 @@ class SupportModule::ProgramService
 
       (1..3).each do |index|
         next_date_and_hour(support_module_week, week_index, @first_support_module)
-
         next if support_module_week.medium.send("body#{index}").blank?
 
-        image = Media::Image.find_by(id: support_module_week.medium.send("image#{index}_id"))
+        rcs_media_id = support_module_week.medium.send("rcs_media#{index}_id")
         redirection_target = RedirectionTarget.find_or_create_by(medium_id: support_module_week.medium.send("link#{index}_id"))
-
         program_message(
           @date,
           @hour,
           @recipients,
           support_module_week.medium.send("body#{index}"),
-          image&.spot_hit_id,
+          rcs_media_id,
           redirection_target&.id
         )
       end
@@ -43,16 +41,14 @@ class SupportModule::ProgramService
       next if support_module_week.additional_medium.nil?
 
       next_date_and_hour(support_module_week, week_index)
-
-      image = Media::Image.find_by(id: support_module_week.additional_medium.image1_id)
+      rcs_media_id = support_module_week.medium.rcs_media1_id
       redirection_target = RedirectionTarget.find_or_create_by(medium_id: support_module_week.additional_medium.link1_id)
-
       program_message(
         @date,
         @hour,
         @recipients,
         support_module_week.additional_medium.body1,
-        image&.spot_hit_id,
+        rcs_media_id,
         redirection_target&.id
       )
     end
@@ -62,13 +58,13 @@ class SupportModule::ProgramService
 
   private
 
-  def program_message(date, hour, recipients, message, image_id, redirection_target_id)
+  def program_message(date, hour, recipients, message, rcs_media_id, redirection_target_id)
     service = ProgramMessageService.new(
       date,
       hour,
       recipients,
       message,
-      image_id,
+      rcs_media_id,
       redirection_target_id
     ).call
     @errors += service.errors
