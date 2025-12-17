6cfca93b9e40819030cd588967a4d32684f24753
aristidebamenou <aristide.bamenou@hotmail.com>
2025-12-17 09:18:41 +0100

    refactor(services): merge AddDisengagementTagService into ChildrenDisengagementService to simplify code structure

--- app/services/child_support/children_disengagement_service.rb
+++ app/services/child_support/children_disengagement_service.rb
@@ -1,31 +1,54 @@
 class ChildSupport::ChildrenDisengagementService
+
+  DISENGAGEMENT_STATUSES = ['KO', 'Ne pas appeler', 'Numéro erroné'].freeze
   MESSAGE = "Bonjour,\n{PRENOM_ACCOMPAGNANTE} a essayé de vous appeler plusieurs fois mais n'a pas réussi à discuter avec vous. Avec 1001mots, quand on n'arrive pas à échanger, l'accompagnement se termine pour que d'autres familles en profitent. Les livres et SMS vont donc s'arrêter bientôt.\nMerci d'avoir participé à ce programme. On vous souhaite de beaux moments avec {PRENOM_ENFANT} !\nEt si vous avez encore 1 minute, dites-nous ici ce que vous avez pensé de 1001mots : https://form.typeform.com/to/fysdS3Sd#st=xxxxx\nL'équipe 1001mots".freeze
 
   attr_reader :errors, :parent_ids
 
-  def initialize(group_id)
+  def initialize(group_id, call_index)
     @group = Group.find(group_id)
     @errors = []
     @message = MESSAGE.dup
     @parents_with_multiple_children = []
     @parents_with_single_child = []
     @parent_ids = []
+    @child_supports =
+      if Group.find(group_id).type_of_support == 'without_calls'
+        []
+      else
+        case call_index
+        when 1
+          ChildSupport.group_id_in(group_id).with_a_child_in_active_group.
+            where(call1_avoid_disengagement_date: nil).
+            where('call1_status IN (?, ?, ?)', 'KO', 'Ne pas appeler', 'Numéro erroné').
+            select { |child_support| child_support.call0_status.in? DISENGAGEMENT_STATUSES }
+        when 2
+          ChildSupport.group_id_in(group_id).with_a_child_in_active_group.
+            where(call2_avoid_disengagement_date: nil).
+            where('call2_status IN (?, ?, ?)', 'KO', 'Ne pas appeler', 'Numéro erroné').
+            select { |child_support| child_support.call1_status.in?(DISENGAGEMENT_STATUSES) || child_support.call0_status.in?(DISENGAGEMENT_STATUSES) }
+        when 3
+          ChildSupport.group_id_in(group_id).with_a_child_in_active_group.
+            where(call3_avoid_disengagement_date: nil).
+            where('call3_status IN (?, ?, ?)', 'KO', 'Ne pas appeler', 'Numéro erroné').
+            select { |child_support| child_support.call2_status.in?(DISENGAGEMENT_STATUSES) || child_support.call1_status.in?(DISENGAGEMENT_STATUSES) || child_support.call0_status.in?(DISENGAGEMENT_STATUSES) }
+        else
+          []
+        end
+      end
   end
 
   def call
     return self if group_without_calls?
 
-    ChildSupport
-      .includes(:children)
-      .where(children: { id: @group.children.map(&:id) })
-      .tagged_with('desengage-2appelsKO')
-      .uniq
-      .each do |child_support|
+    @child_supports.each do |child_support|
         @child_support = child_support
         parent = contactable_parent
         next unless parent
 
         disengage_active_children_in_group
+        child_support.tag_list += ['desengage-2appelsKO']
+        child_support.save!
         add_parent_to_recipients(parent)
       end
 
