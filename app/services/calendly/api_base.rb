379a4fee66911a007411786fa24bcb6aae969a0f
aristidebamenou <aristide.bamenou@hotmail.com>
2025-12-23 13:09:28 +0100

    feat(calendly): add ApiBase service with base URL and authentication

---
+++ app/services/calendly/api_base.rb
@@ -0,0 +1,18 @@
+module Calendly
+
+  class ApiBase
+
+    BASE_URL = 'https://api.calendly.com'.freeze
+    EVENT_TYPES_ENDPOINT = '/event_types'.freeze
+
+    protected
+
+    def http_client_with_auth
+      HTTP.auth("Bearer #{ENV.fetch('CALENDLY_TOKEN')}")
+    end
+
+    def build_url(endpoint, params = '')
+      "#{BASE_URL}#{endpoint}#{params}"
+    end
+  end
+end
