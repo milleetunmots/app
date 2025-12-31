92686a22b97570c2139b82919f2502ba4d7a68ce
Maxime Martin <dev@maxmartin.fr>
2025-12-31 13:20:16 +0100

    fix img decorator and add locales

--- app/helpers/active_admin/images_helper.rb
+++ app/helpers/active_admin/images_helper.rb
@@ -23,7 +23,7 @@ module ActiveAdmin::ImagesHelper
     # tag itself
 
     if Rails.application.config.active_storage.service == 'minio'
-      url = source.service_url.gsub('http://minio:9000', "https://#{ENV['DEFAULT_HOSTNAME']}").split('?').first
+      url = source.blob.service_url.gsub('http://minio:9000', "https://#{ENV['DEFAULT_HOSTNAME']}").split('?').first
       tag = image_tag url, options.merge(style: style.join)
     else
       tag = image_tag source, options.merge(style: style.join)
