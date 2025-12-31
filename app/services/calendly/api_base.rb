18dd1216b57a256cf1915147ac3f97d5baaec04d
aristidebamenou <aristide.bamenou@hotmail.com>
2025-12-31 13:20:47 +0100

    feat(calendly): add organization URI and pagination support to ApiBase

--- app/services/calendly/api_base.rb
+++ app/services/calendly/api_base.rb
@@ -1,8 +1,12 @@
 module Calendly
   class ApiBase
 
+    ORGANIZATION_URI = ENV['CALENDLY_ORGANIZATION_URI']
+
     BASE_URL = 'https://api.calendly.com'.freeze
+
     EVENT_TYPES_ENDPOINT = '/event_types'.freeze
+    LIST_ORGANIZATION_MEMBERSHIPS_ENDPOINT = '/organization_memberships'.freeze
 
     protected
 
@@ -12,6 +16,13 @@ module Calendly
 
     def build_url(endpoint, params = '')
       "#{BASE_URL}#{endpoint}#{params}"
+    end
+
+    def next_page_link(body)
+      pagination = body['pagination']
+      return nil unless pagination && pagination['next_page']
+
+      pagination['next_page']
     end
   end
 end
