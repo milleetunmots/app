efe4c8a7eaa6cbc0b4b1579408a26d9ea07097bb
aristidebamenou <aristide.bamenou@hotmail.com>
2026-01-20 12:02:59 +0100

    feat(send_rcs_service) : create_event after rcs sending

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_01_14_150303) do
+ActiveRecord::Schema[7.0].define(version: 2026_01_20_104116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -493,9 +493,11 @@ ActiveRecord::Schema[7.0].define(version: 2026_01_14_150303) do
     t.string "aircall_message_id"
     t.string "message_provider"
     t.integer "workshop_time_slot"
+    t.string "spot_hit_rcs_id"
     t.index ["discarded_at"], name: "index_events_on_discarded_at"
     t.index ["quit_group_child_id"], name: "index_events_on_quit_group_child_id"
     t.index ["related_type", "related_id"], name: "index_events_on_related_type_and_related_id"
+    t.index ["spot_hit_rcs_id"], name: "index_events_on_spot_hit_rcs_id"
     t.index ["type", "spot_hit_message_id"], name: "index_events_on_type_and_spot_hit_message_id"
     t.index ["type"], name: "index_events_on_type"
     t.index ["workshop_id"], name: "index_events_on_workshop_id"
