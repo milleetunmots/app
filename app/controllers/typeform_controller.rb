c572c187deb3106e87103f8884b79bce4bdbc08e
Maxime Martin <dev@maxmartin.fr>
2026-02-04 12:09:57 +0100

    fix(typeforms) : return 200 OK without rollbar if form id is for call3

--- app/controllers/typeform_controller.rb
+++ app/controllers/typeform_controller.rb
@@ -35,8 +35,8 @@ class TypeformController < ApplicationController
           Typeform::MidwayFormService.new(params[:form_response]).call
         when CALL_ZERO_GOALS_TYPEFORM_ID
           Typeform::CallGoalsFormService.new(params[:form_response], 0).call
-        # when CALL_THREE_SPEAKING_TYPEFORM_ID, CALL_THREE_OBSERVING_TYPEFORM_ID
-        #   Typeform::CallGoalsFormService.new(params[:form_response], 3).call
+        when CALL_THREE_SPEAKING_TYPEFORM_ID, CALL_THREE_OBSERVING_TYPEFORM_ID
+          return head :ok
         when INITIAL_TYPEFORM_ID
           Typeform::InitialFormService.new(params[:form_response]).call
         when UPDATING_ADDRESS_TYPEFORM_ID, UPSTREAM_ADDRESS_UPDATING_TYPEFORM_ID, UPDATING_ADDRESS_FOR_PARTNERS_TYPEFORM_ID
@@ -44,7 +44,7 @@ class TypeformController < ApplicationController
         when CAF_SUBSCRIPTION_TYPEFORM_ID
           Typeform::AddCafSubscriptionTagService.new(params[:form_response]).call
         end
-      Rollbar.error('Typeform service error', service: service.class.to_s, errors: service.errors, form_response: params[:form_response]) unless service&.errors&.empty?
+      Rollbar.error('Typeform service error', service: service.class.to_s, errors: service.errors, form_response: params[:form_response]) unless service.errors.empty?
     else
       Rollbar.error('Typeform with unknown id', typeform_id: params[:form_response][:form_id], form_response: params[:form_response])
     end
