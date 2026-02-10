6008ab753c37a7995cfda03aa380abea9ebdcd23
aristidebamenou <aristide.bamenou@hotmail.com>
2026-02-10 19:17:18 +0100

    feat(dashboard): display automatic SMS settings panel on dashboard for callers/animators

--- app/admin/admin_user_can_send_automatic_sms_parameter.rb
+++ app/admin/admin_user_can_send_automatic_sms_parameter.rb
@@ -3,24 +3,7 @@ ActiveAdmin.register_page 'Réglages' do
 
   content do
     render partial: 'admin/admin_user_can_send_automatic_sms_parameters/modal'
-    panel 'Paramètres des rendez-vous' do
-      div style: 'display: flex; align-items: center;' do
-        i class: 'far fa-check-circle', style: 'margin-right: 8px; color: #555; font-size: 20px;'
-        if current_active_admin_user.can_send_automatic_sms
-          span "Toutes les familles reçoivent un SMS automatique de prise de RDV avant chaque session d'appel.", style: 'margin-right: 30px;'
-        else
-          span style: 'margin-right: 30px;' do
-            b 'Aucune'
-            span " famille ne reçoit de SMS automatique de prise de RDV avant chaque session d'appel."
-          end
-        end
-        if current_active_admin_user.can_send_automatic_sms
-          a 'Désactiver les SMS de RDV', id: 'toggle-automatic-sms-button', class: 'button'
-        else
-          a 'Activer les SMS de RDV', href: admin_reglages_toggle_automatic_sms_path, 'data-method': 'post', class: 'button'
-        end
-      end
-    end
+    render partial: 'admin/admin_user_can_send_automatic_sms_parameters/settings_panel', locals: { on_dashboard: false }
   end
 
   page_action :toggle_automatic_sms, method: :post do
--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,6 +3,7 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
+    render partial: 'admin/admin_user_can_send_automatic_sms_parameters/settings_panel', locals: { on_dashboard: true } if current_active_admin_user.user_role.in?(%w[caller animator])
     render 'tasks'
     render 'paper_trail'
   end
