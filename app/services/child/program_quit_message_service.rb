72b636d34d43f4074569d3ffa8eb0f7a0022a3e2
Maxime Martin <dev@maxmartin.fr>
2026-03-16 11:14:09 +0100

    fix(messages) : format_data_for_spot_hit refactor (#1262)

--- app/services/child/program_quit_message_service.rb
+++ app/services/child/program_quit_message_service.rb
@@ -1,24 +1,24 @@
 class Child::ProgramQuitMessageService < ProgramMessageService
 
-  def format_data_for_spot_hit
+  def format_data_for_spot_hit(_rcs = false)
     @recipient_data = {}
     @child_ids.each do |child_id|
       child = Child.find(child_id)
-      @recipient_data[child.parent1_id.to_s] = {}
-      @recipient_data[child.parent1_id.to_s]['QUIT_LINK'] = Rails.application.routes.url_helpers.edit_child_url(
+      @recipient_data[child.parent1.phone_number] = {}
+      @recipient_data[child.parent1.phone_number]['QUIT_LINK'] = Rails.application.routes.url_helpers.edit_child_url(
         id: child_id,
         security_code: child.security_code
       )
 
-      @event_params[child.parent1_id.to_s] = { quit_group_child_id: child_id }
+      @event_params[child.parent1_id] = { quit_group_child_id: child_id }
       next unless child.parent2
 
-      @recipient_data[child.parent2_id&.to_s] = {}
-      @recipient_data[child.parent2_id&.to_s]['QUIT_LINK'] = Rails.application.routes.url_helpers.edit_child_url(
+      @recipient_data[child.parent2.phone_number] = {}
+      @recipient_data[child.parent2.phone_number]['QUIT_LINK'] = Rails.application.routes.url_helpers.edit_child_url(
         id: child_id,
         security_code: child.security_code
       )
-      @event_params[child.parent2_id.to_s] = { quit_group_child_id: child_id }
+      @event_params[child.parent2_id] = { quit_group_child_id: child_id }
     end
   end
 end
