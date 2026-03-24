07ca42e8ad61b59ca8b06a500ea4b0cc82ab86ff
aristidebamenou <aristide.bamenou@hotmail.com>
2026-03-24 16:44:55 +0100

    fix(export_books_v2_service): generate summary Excel file for unprogrammed module selection

--- app/services/child/export_books_v2_service.rb
+++ app/services/child/export_books_v2_service.rb
@@ -1,9 +1,12 @@
 require 'zip'
+require 'fast_excel'
 
 class Child
   class ExportBooksV2Service
     attr_reader :errors
     attr_reader :zip_file
+
+    SUMMARY_COLUMNS = ['Nom du module', 'Âges', 'Effectif', 'EAN', 'Livre', 'Cohorte', 'Module'].freeze
 
     def initialize(group_ids: nil)
       @errors = []
@@ -21,11 +24,56 @@ class Child
 
       @errors << 'Aucun choix de module à programmer n\'a été trouvé' if excel_files.empty?
 
-      create_zip_file(excel_files) if @errors.empty?
+      if @errors.empty?
+        excel_files << { filename: 'choix-modules.xlsx', file: generate_modules_summary }
+        create_zip_file(excel_files)
+      end
+
       self
     end
 
     private
+
+    def generate_modules_summary
+      workbook = FastExcel.open
+      worksheet = workbook.add_worksheet
+      header_format = workbook.add_format(bold: true, bg_color: :'#70AD47', font_color: :white)
+      worksheet.append_row(SUMMARY_COLUMNS, header_format)
+
+      Group.where(id: @group_ids).each do |group|
+        child_and_parent1_ids = group.children.joins(:child_support)
+                                     .where(child_support: { address_suspected_invalid_at: nil })
+                                     .active_group.pluck(:id, :parent1_id)
+
+        summary = {}
+
+        child_and_parent1_ids.each do |child_id, parent1_id|
+          csm = ChildrenSupportModule.with_support_module
+                                     .where(is_programmed: false)
+                                     .find_by(child_id: child_id, parent_id: parent1_id)
+          next unless csm
+
+          module_num = csm.module_index - 1
+          module_label = "Module #{module_num}"
+          module_name = csm.support_module.name
+          age_ranges = csm.support_module.decorate.display_age_ranges.to_sym
+
+          key = [module_label, module_name, age_ranges]
+          summary[key] ||= { count: 0 }
+          summary[key][:count] += 1
+          summary[key][:book_ean] ||= csm.book_ean.presence || csm.support_module.book&.ean
+          summary[key][:book_title] ||= csm.book_title.presence || csm.support_module.book&.title
+        end
+
+        summary.each do |(module_label, module_name, age_key), values|
+          worksheet.append_row([module_name, age_key, values[:count], values[:book_ean], values[:book_title], group.name, module_label])
+        end
+      end
+
+      worksheet.set_columns_width(0, 3, 25)
+
+      workbook
+    end
 
     def find_children_lists
       children_list_sorted_by_module = {}
