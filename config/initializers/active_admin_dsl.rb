afafc23f627f060268c143f675c561e14ef6cc1e
aristidebamenou <aristide.bamenou@hotmail.com>
2026-03-30 11:28:44 +0200

    fix(active_admin): update index DSL to render partial above scopes instead of table

--- config/initializers/active_admin_dsl.rb
+++ config/initializers/active_admin_dsl.rb
@@ -12,11 +12,11 @@ require 'active_admin/tasks/dsl'
 ::ActiveAdmin::DSL.send :include, ActiveAdmin::Tags::DSL
 ::ActiveAdmin::DSL.send :include, ActiveAdmin::Tasks::DSL
 
-# Allows rendering a partial above the index table (even when the collection is empty).
+# Allows rendering a partial above the scopes on the index page (even when the collection is empty).
 # Usage: set @before_index_partial in a controller before_action.
 # Example: @before_index_partial = { partial: 'path/to/partial', locals: { key: value } }
 module ActiveAdminBeforeIndexPartial
-  def build_collection
+  def main_content
     if (partial_info = assigns[:before_index_partial])
       text_node helpers.render(partial: partial_info[:partial], locals: partial_info.fetch(:locals, {}))
     end
