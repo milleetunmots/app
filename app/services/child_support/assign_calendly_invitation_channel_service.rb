0bf7cf1014a05526361c618f1d3ee423a5eea1be
aristidebamenou <aristide.bamenou@hotmail.com>
2026-06-02 15:31:52 +0200

    refactor(child_support): extract tag assignment logic into private method in `AssignCalendlyInvitationChannelService`

--- app/services/child_support/assign_calendly_invitation_channel_service.rb
+++ app/services/child_support/assign_calendly_invitation_channel_service.rb
@@ -4,49 +4,22 @@ class ChildSupport::AssignCalendlyInvitationChannelService
   SMS_TAG = 'ab_testing_calendly_invite_sms'.freeze
   CHANNELS = [RCS_TAG, SMS_TAG].freeze
 
-  attr_reader :errors, :rcs_child_support_ids, :sms_child_support_ids
+  attr_reader :errors
 
   def initialize(child_supports)
-    @child_supports = child_supports.to_a
+    @child_supports = child_supports.with_a_child_in_active_group.not_tagged_with_all(CHANNELS).distinct.to_a.group_by(&:supporter_id)
     @errors = []
     @rcs_child_support_ids = []
     @sms_child_support_ids = []
   end
 
   def call
-    @child_supports.group_by(&:supporter_id).each_value do |_, child_supports|
-      child_supports = child_supports.not_tagged_with_all(CHANNELS).shuffle
-      rcs_child_support, sms_child_support = child_supports.each_slice((child_supports.count / 2.0).ceil).to_a
+    @child_supports.each_value do |child_supports|
+      child_supports = child_supports.shuffle
+      rcs_child_supports, sms_child_supports = child_supports.each_slice((child_supports.count / 2.0).ceil).to_a
 
-      rcs_child_support.each do |child_support|
-        next if child_support.tag_list.include?(RCS_TAG) || child_support.tag_list.include?(SMS_TAG)
-
-        child_support.tag_list.add(RCS_TAG)
-        if child_support.save
-          @rcs_child_support_ids << child_support.id
-        else
-          @errors << {
-            service: 'ChildSupport::AssignCalendlyInvitationChannelService',
-            child_support_id: child_support.id,
-            error: child_support.errors.full_messages
-          }
-        end
-      end
-
-      sms_child_support.each do |child_support|
-        next if child_support.tag_list.include?(RCS_TAG) || child_support.tag_list.include?(SMS_TAG)
-
-        child_support.tag_list.add(SMS_TAG)
-        if child_support.save
-          @sms_child_support_ids << child_support.id
-        else
-          @errors << {
-            service: 'ChildSupport::AssignCalendlyInvitationChannelService',
-            child_support_id: child_support.id,
-            error: child_support.errors.full_messages
-          }
-        end
-      end
+      add_tag(rcs_child_supports, RCS_TAG, @rcs_child_support_ids)
+      add_tag(sms_child_supports, SMS_TAG, @sms_child_support_ids)
     end
 
     if @errors.empty?
@@ -62,5 +35,26 @@ class ChildSupport::AssignCalendlyInvitationChannelService
       )
     end
     self
+  end
+
+  private
+
+  def add_tag(child_supports, tag, ids)
+    return if child_supports.nil?
+
+    child_supports.each do |child_support|
+      next if child_support.tag_list.include?(RCS_TAG) || child_support.tag_list.include?(SMS_TAG)
+
+      child_support.tag_list.add(tag)
+      if child_support.save
+        ids << child_support.id
+      else
+        @errors << {
+          service: 'ChildSupport::AssignCalendlyInvitationChannelService',
+          child_support_id: child_support.id,
+          error: child_support.errors.full_messages
+        }
+      end
+    end
   end
 end
