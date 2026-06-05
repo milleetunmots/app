7488763a4f5411e992c986f43b3b1a2be21c515e
aristidebamenou <aristide.bamenou@hotmail.com>
2026-06-05 09:34:26 +0200

    fix(validation): update error messages for call session date override validation

--- app/models/call_session_date_override.rb
+++ app/models/call_session_date_override.rb
@@ -42,7 +42,7 @@ class CallSessionDateOverride < ApplicationRecord
   def start_date_before_or_equal_end_date
     return if start_date.blank? || end_date.blank?
 
-    errors.add(:base, :invalid, message: 'La date de début doit être antérieure ou égale à la date de fin') if start_date > end_date
+    errors.add(:base, :invalid, message: 'La date de fin doit être après la date de début.') if start_date > end_date
   end
 
   def dates_within_group_call_session
@@ -52,11 +52,11 @@ class CallSessionDateOverride < ApplicationRecord
     group_end = group.send(:"call#{call_session}_end_date")
 
     if start_date.present? && group_start.present? && start_date < group_start
-      errors.add(:base, :invalid, message: "La date de début doit être ultérieure au #{I18n.l(group_start)}")
+      errors.add(:base, :invalid, message: " La plage de RDV ne peut pas commencer avant le début de la session d’appel.")
     end
 
     if end_date.present? && group_end.present? && end_date > group_end
-      errors.add(:base, :invalid, message: "La date de fin doit être antérieure au #{I18n.l(group_end)}")
+      errors.add(:base, :invalid, message: "La plage de RDV ne peut pas se terminer après la fin de la session d'appel.")
     end
   end
 end
