8406c44b34b9da7c7cce12b19349e5f16d595264
aristidebamenou <aristide.bamenou@hotmail.com>
2026-06-23 17:58:26 +0200

    fix(spot_hit): add basic RCS fallback for short messages without variables

--- app/services/program_message_service.rb
+++ app/services/program_message_service.rb
@@ -76,7 +76,16 @@ class ProgramMessageService
     when 'spothit'
       service =
         if @rcs_media_id.nil?
-          SpotHit::SendSmsService.new(@recipient_data, @planned_timestamp, @message, workshop_id: @workshop_id, event_params: @event_params).call
+          if @variables.empty? && @message.length <= 160
+            SpotHit::SendRcsService.new(
+              recipients: @recipient_data,
+              planned_timestamp: @planned_timestamp,
+              fallback_message: @message,
+              basic: true
+            ).call
+          else
+            SpotHit::SendSmsService.new(@recipient_data, @planned_timestamp, @message, workshop_id: @workshop_id, event_params: @event_params).call
+          end
         else
           SpotHit::SendRcsService.new(
             recipients: @recipient_data,
@@ -84,8 +93,7 @@ class ProgramMessageService
             media_id: @rcs_media_id,
             fallback_message: @message
           ).call
-        end
-
+          end
       if service.errors.any?
         @errors = service.errors
       elsif @invalid_parent_ids.any?
