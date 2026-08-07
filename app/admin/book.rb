88373deb63bbd79f588b69e7c5db5c587ac7b22e
aristidebamenou <aristide.bamenou@hotmail.com>
2026-08-07 07:31:19 +0200

    feat(book_shipment_dates) : input added

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -4,11 +4,26 @@ ActiveAdmin.register Book do
 
   actions :all, except: %i[new edit]
 
+  collection_action :upsert_shipment_date, method: :post do
+    shipment_date = BookShipmentDate.find_or_initialize_by(id: params[:id].presence)
+    shipment_date.date = params[:date]
+
+    if shipment_date.save
+      render json: { ok: true, id: shipment_date.id }
+    else
+      render json: { ok: false, errors: shipment_date.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   # ---------------------------------------------------------------------------
   # INDEX
   # ---------------------------------------------------------------------------
 
   index do
+    div do
+      render 'index_top'
+    end
+
     selectable_column
     id_column
     column :ean
