373faa8ddcf307ec47e3c5b9bb799d57b377f19e
Maxime Martin <dev@maxmartin.fr>
2026-08-10 15:58:10 +0200

    fix(children) : pmi pro email filter

--- app/admin/child.rb
+++ app/admin/child.rb
@@ -97,10 +97,8 @@ ActiveAdmin.register Child do
           collection: proc { source_details_suggestions },
           input_html: { multiple: true, data: { select2: {} } },
           label: "Précisions sur l'origine"
-  filter :children_source_professional_email,
-         as: :select,
-         collection: proc { source_professional_email_suggestions },
-         input_html: { multiple: true, data: { select2: {} } },
+  filter :children_source_professional_email_cont,
+         as: :string,
          label: "Email d'inscription"
   filter :re_enrollment,
          as: :check_boxes,
