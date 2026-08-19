00957d5e8d637b605735a04c161dcfe4c86f9bde
aristidebamenou <aristide.bamenou@hotmail.com>
2026-08-19 13:49:14 +0200

    fix(admin): add and test email-based child filtering logic

--- spec/models/children_source_spec.rb
+++ spec/models/children_source_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: children_sources
+#
+#  id                      :bigint           not null, primary key
+#  details                 :string
+#  professional_email      :string
+#  re_enrollment           :boolean          default(FALSE)
+#  registration_department :integer
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  child_id                :bigint
+#  source_id               :bigint
+#
+# Indexes
+#
+#  index_children_sources_on_child_id   (child_id)
+#  index_children_sources_on_source_id  (source_id)
+#
 require "rails_helper"
 
 RSpec.describe ChildrenSource, type: :model do
